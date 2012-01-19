97a0a21603c137cebb577fbf05b2928fd193ac67
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-01-19 10:49:22 +0000

    Add On/Off switcher for feature set

--- modules/modified/feature_set/feature_set.js
+++ modules/modified/feature_set/feature_set.js
@@ -1,16 +1,21 @@
 jQuery(function($){ 
   $(document).ready( function(){ 
+    //Add class to switcher column
     $("#feature-set-admin-form .form-type-checkbox").each(function() {
       $(this).addClass('switch');  
     });
   
+    //Add switcher before checkbox and hide checkbox
     $("#feature-set-admin-form .form-checkbox").each(function() {
       $html_before = '<label class="cb-enable"><span>On</span></label><label class="cb-disable selected"><span>Off</span></label>';
       
       $(this).before($html_before);
+      $(this).css('opacity',0);
     });
       
-      
+
+    
+    //Manage click on switcher  
     $(".cb-enable").click(function(){
       var parent = $(this).parents('.switch');
       $('.cb-disable',parent).removeClass('selected');
