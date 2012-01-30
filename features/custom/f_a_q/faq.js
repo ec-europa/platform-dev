45d3b4406e20366b322416ab666349fe0f0bb941
degliwe <degliwe@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-01-30 10:08:46 +0000

    Make the FAQ feature conform to specs (expand/collapse all + link create new faq) some view optimisation. Now only add js when needed

--- features/custom/f_a_q/faq.js
+++ features/custom/f_a_q/faq.js
@@ -1,14 +1,36 @@
 (function ($) {
   $(document).ready(function(){
+  
 		//FAQ
-		$(".view-faq dt").click(function(){
-			$(this).next("dd").slideToggle("slow");
+		$(".view-faq .views-field-title").click(function(){
+			$(this).next(".views-field-body").slideToggle("slow");
+			$(this).next(".views-field-field-tags").slideToggle("slow");
 			$(this).toggleClass("active");
 			return false;
 		});	
 		
-		$(".view-faq dt").toggleClass("active");
-		$(".view-faq dt").css("text-decoration","underline");
-		$(".view-faq dd").css("display","none");
+		//init
+		$(".view-faq .views-field-title").toggleClass("active");
+		$(".view-faq .views-field-title").css("text-decoration","underline");
+		$(".view-faq .views-field-body").css("display","none");
+		$(".view-faq .views-field-field-tags").css("display","none");
+		
+		//expand
+		$(".faq_expand").click(function(){
+			$(".view-faq .views-field-body").slideDown("slow");
+			$(".view-faq .views-field-field-tags").slideDown("slow");
+			$(".view-faq .views-field-title").addClass("active");
+			return false;
+		});	
+		
+		//collapse
+		$(".faq_collapse").click(function(){
+			$(".view-faq .views-field-body").slideUp("slow");
+			$(".view-faq .views-field-field-tags").slideUp("slow");
+			$(".view-faq .views-field-title").removeClass("active");
+			return false;
+		});	
+		
+		
 	});
 })(jQuery);
