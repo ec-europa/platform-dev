1351dac909e4471b9b05dff0acfedfdcff797210
colleln <colleln@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-02-02 10:23:56 +0000

    Set the UNIX end line & update the files including - Management of multiple patch files - Management of verbose mode
    Change of the .path file in order to be coherent with the methodology of Joinup

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage="Installation of multisite instance\n
-Syntax : $(basename $0) [ARGS] INSTANCE\n
+Syntax : $(basename $0) [ARGS] SITE-NAME\n
 \t-?,-h, --help\t\tPrint this message\n\n
 \t-v, --verbose\t\tSet the script in verbose mode
 Connection information\n
@@ -10,15 +10,15 @@ Connection information\n
 \t-H, --db-host\t\tSet the database host\n\n
 \t-P, --db-port\t\tSet the database port\n\n
 Configuration of the site\n
+\t-r, --web-root\t\tDefine the web root\n
 \t-a, --account\t\tDefine the account name for the administrator\n
 \t-e, --account-email\tDefine the email address for the administrator\n
-\t-n, --site-name\t\tDefine the site name\n
 \t-m, --site-email\t\tDefine the site email\n
 \t-b, --base-url\t\tDefine the base URL of the site\n"
 
 
 #Default values
-webroot='/var/www'
+webroot='/var/www/html'
 db_user='root'
 db_pass=''
 db_host='localhost'
@@ -26,54 +26,65 @@ db_port=3306
 account_name='admin'
 account_pass='pass'
 account_mail='EMAIL@ADDRESS.COM'
-site_name='SITE NAME'
 site_mail='EMAIL@ADDRESS.COM'
 baseurl='www.site.com'
-verbose=false
+verbose=0
+
+site_name=$BASH_ARGV
+__echo "Print SITE_NAME = ${site_name}"
 
 while getopts "u:p:H:P:a:e:n:b:vh?-:" option; do
-	#Management of the --options
-	if [ "$option" = "-" ]; then
-		case $OPTARG in
-			help) option=h ;;
-			verbose) option=v ;;
-			dbuser) option=u ;;
-			dbpass) option=p ;;
-			dbhost) option=H ;;
-			dbport) option=P ;;
-			account) option=a ;;
-			account-email) option=e ;;
-			site-name) option=n ;;
-			site-email) option=m ;;
-			base-url) option=b ;;
-			*) 
-				echo "[ERROR] Unknown option --$OPTARG"
-				exit 1
-				;;
-		esac
-	fi
-	case $option in
-		u) db_user=$OPTARG ;;
-		p) db_pass=$OPTARG ;;
-		H) db_host=$OPTARG ;;
-		P) db_port=$OPTARG ;;
-		a) account=$OPTARG ;;
-		e) account_mail=$OPTARG ;;
-		n) site_name=$OPTARG ;;
-		m) site_mail=$OPTARG ;;
-		b) baseurl=$OPTARG ;;
-		v) verbose=true ;;
-		\?|h)
-			echo -e $usage
-			exit 0
-			;;
-		:)
-			echo "[ERROR] Missing arguments for -$OPTARG"
-			exit 1
-			;;
-		?)
-			echo "[ERROR] Unknown option -$option"
-			exit 1
-			;;
-	esac
+        #Management of the --options
+        if [ "$option" = "-" ]; then
+                case $OPTARG in
+                        help) option=h ;;
+                        verbose) option=v ;;
+                        dbuser) option=u ;;
+                        dbpass) option=p ;;
+                        dbhost) option=H ;;
+                        dbport) option=P ;;
+                        account) option=a ;;
+                        account-email) option=e ;;
+						web-root) option=r ;;
+                        site-email) option=m ;;
+                        base-url) option=b ;;
+                        *)
+                                echo "[ERROR] Unknown option --$OPTARG"
+                                exit 1
+                                ;;
+                esac
+        fi
+        case $option in
+                u) db_user=$OPTARG ;;
+                p) db_pass=$OPTARG ;;
+                H) db_host=$OPTARG ;;
+                P) db_port=$OPTARG ;;
+                a) account=$OPTARG ;;
+                e) account_mail=$OPTARG ;;
+				r) webroot=$OPTARG ;;
+                m) site_mail=$OPTARG ;;
+                b) baseurl=$OPTARG ;;
+                v) verbose=1 ;;
+                \?|h)
+                        echo -e $usage
+                        exit 0
+                        ;;
+                :)
+                        echo "[ERROR] Missing arguments for -$OPTARG"
+                        exit 1
+                        ;;
+                ?)
+                        echo "[ERROR] Unknown option -$option"
+                        exit 1
+                        ;;
+        esac
 done
+
+db_url="mysqli://${db_user}:${db_pass}@${db_host}:${db_port}/${site_name}"
+
+
+function __echo {	
+	if [ "${verbose}" = 1 ] ; then
+		echo $1
+	fi
+}
--- install.sh
+++ install.sh
@@ -2,42 +2,59 @@
 
 source config.sh
 
+current_dir=$(pwd)
+__echo "Set current directory to ${current_dir}"
+working_dir="${current_dir}/${site_name}"
+__echo "Set working directory to ${working_dir}"
 
 #build the drupal instance
-drush make profiles/multisite_drupal_core/build.make $1
+drush make profiles/multisite_drupal_core/build.make ${site_name}
 
-mysql -u $db_user -p "$db_pass" -e "drop database $1;"
-mysql -u $db_user -p "$db_pass" -e "create database $1;"
+mysql -h ${db_host} -P ${db_port} -u $db_user --password="$db_pass" -e "drop database ${site_name};"
+mysql -h ${db_host} -P ${db_port} -u $db_user --password="$db_pass" -e "create database ${site_name};"
 
-chmod -R 777 $1/sites/default
-cp -R profiles/multisite_drupal_core $1/profiles
-cp -R profiles/subsite_standard $1/profiles
-cp -R profiles/subsite_communities $1/profiles
-cp -R modules $1/sites/all
-cp -R features $1/sites/all/modules
-cp -R themes $1/sites/all
-cp -R files/ $1/sites/default/files/
+chmod -R 777 ${site_name}/sites/default
+cp -R profiles/multisite_drupal_core ${site_name}/profiles
+cp -R profiles/subsite_standard ${site_name}/profiles
+cp -R profiles/subsite_communities ${site_name}/profiles
+cp -R modules ${site_name}/sites/all
+cp -R features ${site_name}/sites/all/modules
+cp -R themes ${site_name}/sites/all
+cp -R files/ ${site_name}/sites/default/files/
 
 
-#apply patches
-patch -b $1/sites/all/modules/contributed/workbench_moderation/workbench_moderation.module patches/workbench_moderation.patch
+# we assume the script is in the patches directory
+patch_dir=$(readlink -f patches)
 
+# we assume the patch directory is located in patches, i.e. one level below the Drupal root directory
+__echo "Applying patches to from ${patch_dir} to ${site_name}..."
+
+cd ${site_name}
+
+if [ $? != 0 ] ; then
+	__echo "Unable to change directory to ${site_name}" 
+	exit 20
+fi
+
+
+for patch_file in "${patch_dir}/"*.patch "${patch_dir}/"*.diff; do
+	test -f "${patch_file}" || continue 
+	__echo "Attempting to apply ${patch_file}..."
+	__echo `patch -p0 -b -i "${patch_file}"`
+done
 
 #install and configure the drupal instance
-cd $1
-drush si multisite_drupal_core --db-url=$db_url/$1 --account-name=$account_name --account-pass=$account_pass --site-name=$1 --site-mail=$site_mail
+drush si multisite_drupal_core --db-url=$db_url --account-name=$account_name --account-pass=$account_pass --site-name=${site_name} --site-mail=$site_mail
 
-mkdir sites/default/files/private_files
-chmod -R 777 sites/default/files
+mkdir "${working_dir}/sites/default/files/private_files"
+chmod -R 777 "${working_dir}/sites/default/files"
 
-mkdir sites/all/libraries
+mkdir "${working_dir}/sites/all/libraries"
 #cd sites/all/libraries
-cd sites/all/modules/contributed/ckeditor
-rmdir ckeditor
-wget http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.6.2/ckeditor_3.6.2.tar.gz
-tar xvf ckeditor_3.6.2.tar.gz
-rm ckeditor_3.6.2.tar.gz
+cd "${working_dir}/sites/all/modules/contributed/ckeditor"
+rm -rf "${working_dir}/sites/all/modules/contributed/ckeditor/ckeditor"
+wget -P "${working_dir}/sites/all/modules/contributed/ckeditor/" http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.6.2/ckeditor_3.6.2.tar.gz
+tar xvzf "${working_dir}/sites/all/modules/contributed/ckeditor/ckeditor_3.6.2.tar.gz"
+rm "${working_dir}/sites/all/modules/contributed/ckeditor/ckeditor_3.6.2.tar.gz"
 
-cd ../../../../../..
-mv $1 $webroot
-
+mv "${current_dir}/{site_name}" $webroot
