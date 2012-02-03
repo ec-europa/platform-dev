4bd3abfd6bcff139d7f802205f37ba391bf9097d
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-02-03 12:57:09 +0000

    Update on/off switcher

--- modules/modified/feature_set/feature_set.js
+++ modules/modified/feature_set/feature_set.js
@@ -9,34 +9,38 @@ jQuery(function($){
     $("#feature-set-admin-form .form-checkbox").each(function() {
       //Check if the feature has been enabled
       if ($(this).is(':checked')) {
-        var html_before = '<label class="cb-enable selected"><span>On</span></label><label class="cb-disable"><span>Off</span></label>';
-        var row = $(this).parents('tr');
-        row.addClass('gradient');
+        var html_before = '<label class="cb-enable selected"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable"><span><i class="icon-remove icon-white"></i></span></label>';
+        /*var row = $(this).parents('tr');
+        row.addClass('gradient');*/
       } else {
-        var html_before = '<label class="cb-enable"><span>On</span></label><label class="cb-disable selected"><span>Off</span></label>';
+        var html_before = '<label class="cb-enable"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable selected"><span><i class="icon-remove icon-white"></i></span></label>';
       }     
       
       $(this).before(html_before);
       $(this).css('opacity',0);
     });
-      
 
+    //Manage click on switcher 
+    $('.switch').click(function() {
     
-    //Manage click on switcher  
-    $(".cb-enable").click(function(){
-      if ($(this).parents('.form-disabled').size()==0) {
-        var parent = $(this).parents('.switch');
-        $('.cb-disable',parent).removeClass('selected');
-        $(this).addClass('selected');
-        $('.form-checkbox',parent).attr('checked', true);
-      }
-    });
-    $(".cb-disable").click(function(){
-      if ($(this).parents('.form-disabled').size()==0) {
-        var parent = $(this).parents('.switch');
-        $('.cb-enable',parent).removeClass('selected');
-        $(this).addClass('selected');
-        $('.form-checkbox',parent).attr('checked', false);
+      //check if button is disabled
+      if (!$(this).is('.form-disabled')) {    
+      
+        //add pending status
+        $(this).addClass('pending');
+        
+        //change button
+        $(this).children('label').each(function() {
+          $(this).toggleClass('selected');
+        });
+        
+        //check or uncheck checkbox
+        checkbox = $(this).children('.form-checkbox');
+        if (checkbox.attr('checked')) {
+          checkbox.attr('checked', false);
+        } else {
+          checkbox.attr('checked', true);
+        }        
       }
     });
   });
