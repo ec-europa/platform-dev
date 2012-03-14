caf866ee067af043b94d731518e02e9df655db20
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-03-14 13:35:26 +0000

    -MULTISITE-100

--- sites/all/modules/modified/feature_set/feature_set.js
+++ sites/all/modules/modified/feature_set/feature_set.js
@@ -1,57 +1,41 @@
 jQuery(function($){ 
   $(document).ready( function(){ 
     //Add class to switcher column
-    $("#feature-set-admin-form .form-type-checkbox").each(function() {
-      $(this).addClass('switch');  
-    });
+    $("#feature-set-admin-form .form-type-checkbox").addClass('switch');
     
     //Add class to row if button is disabled
-    $("#feature-set-admin-form tr").each(function() {
-      if ($(this).find('.switch').is('.form-disabled')) {
-        $(this).addClass('form-disabled');
-      }
-    });  
+    $("#feature-set-admin-form tr .switch")
+      .filter('.form-disabled')
+      .closest('tr')
+      .addClass('form-disabled');
   
     //Add switcher before checkbox and hide checkbox
-    $("#feature-set-admin-form .form-checkbox").each(function() {
-      $this = $(this);
-      //Check if the feature has been enabled
-      if ($this.is(':checked')) {
-        var html_before = '<label class="cb-enable selected"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable"><span><i class="icon-remove icon-white"></i></span></label>';
-      } else {
-        var html_before = '<label class="cb-enable"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable selected"><span><i class="icon-remove icon-white"></i></span></label>';
-      }     
-      
-      $this
-        .before(html_before)
-        .css('opacity',0);
-    });
+    $("#feature-set-admin-form .form-checkbox")
+      .css('opacity',0)
+      .filter(':checked')
+        .before('<label class="cb-enable selected"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable"><span><i class="icon-remove icon-white"></i></span></label>')
+      .end()
+      .not(':checked')
+        .before('<label class="cb-enable"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable selected"><span><i class="icon-remove icon-white"></i></span></label>');
 
     //Manage click on a row
     $('#feature-set-admin-form tr').on('click', function() {
-      //get switcher
-      var $this = $(this),
-          switcher = $this.find('.switch');          
-      
-      //check if button is disabled
-      if (!($this.is('.form-disabled'))) {    
-      
-        //add pending status
-        switcher.toggleClass('pending');
-        
-        //change button
-        switcher.children('label').each(function() {
-          $(this).toggleClass('selected');
-        });
-        
-        //check or uncheck checkbox
-        checkbox = switcher.children('.form-checkbox');
-        if (checkbox.attr('checked')) {
-          checkbox.attr('checked', false);
-        } else {
-          checkbox.attr('checked', true);
-        }        
-      }
+      $(this)
+        .not('.form-disabled')
+          .find('.switch')
+            .toggleClass('pending')
+            .children('label')
+              .toggleClass('selected')
+            .end()
+            .children('.form-checkbox')
+              .each(function() {
+                $this = $(this);
+                if ($this.is(':checked')) {
+                  $this.attr('checked', false);
+                } else {
+                  $this.attr('checked', true);
+                }               
+              });               
     });
   });
 });
