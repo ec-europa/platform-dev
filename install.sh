15c3e2950b32d908ecae3fcb438f49ad0d399829
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-05-03 12:57:07 +0000

    MULTISITE-419

--- install.sh
+++ install.sh
@@ -178,7 +178,7 @@ rm "${working_dir}/sites/all/modules/contributed/ckeditor_3.6.2-3_for_drupal_7.z
 cd "${working_dir}/sites/all"
 #mkdir libraries
 cd "${working_dir}/sites/all/libraries"
-wget -P "${working_dir}/sites/all/libraries/" http://sourceforge.net/projects/tcpdf/files/latest/download -O tcpdf.zip
+wget -P "${working_dir}/sites/all/libraries/" http://sourceforge.net/projects/tcpdf/files/latest/download?source=files -O tcpdf.zip
 unzip "tcpdf.zip"
 rm "${working_dir}/sites/all/libraries/tcpdf.zip"
 
