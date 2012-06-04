d11dc1aec175c8f65e5cada90584ac7fe1a85dcd
degliwe <degliwe@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-06-04 10:46:00 +0000

    MULTISITE-475

--- sites/all/themes/ec_default/template.php
+++ sites/all/themes/ec_default/template.php
@@ -91,9 +91,9 @@ function ec_default_preprocess_html(&$variables) {
   $variables['head_title'] = variable_get('site_name') . ' - ' . t('European Commission');
 
   // Add twitter bootsrap
-  //drupal_add_css(path_to_theme() . '/bootstrap/css/bootstrap.min.css', array('group' => CSS_THEME));
+  drupal_add_css(path_to_theme() . '/bootstrap/css/bootstrap.min.css', array('group' => CSS_THEME));
   //drupal_add_css(path_to_theme() . '/bootstrap/bootstrap.less', array('group' => CSS_THEME));
-  // drupal_add_js(path_to_theme() . '/bootstrap/js/bootstrap-dropdown.js', array('scope' => 'footer', 'weight' => 1));
+  //drupal_add_js(path_to_theme() . '/bootstrap/js/bootstrap-dropdown.js', array('scope' => 'footer', 'weight' => 1));
   //drupal_add_js(path_to_theme() . '/bootstrap/js/bootstrap-tab.js', array('scope' => 'footer', 'weight' => 2));
   //drupal_add_js(path_to_theme() . '/bootstrap/js/bootstrap-modal.js', array('scope' => 'footer', 'weight' => 3));
   //drupal_add_js(path_to_theme() . '/bootstrap/js/bootstrap-transition.js', array('scope' => 'footer', 'weight' => 4));
