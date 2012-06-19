eb73bd0729e56bb7d2258f9a6dc8877e5219ecc4
emboulath <emboulath@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-06-19 07:06:00 +0000

    MULTISITE-480 : modify Features to be set at status Default and fix Splashcreen

--- sites/all/modules/features/custom/splash_screen/splash.js
+++ sites/all/modules/features/custom/splash_screen/splash.js
@@ -1,13 +1,56 @@
 (function ($) {
-  $(document).ready(function(){
-  
-    $('ul.languages li a').click(function() {
-	  $lg = $(this).find("span").html();
+    $(document).ready(function(){
+        var ckie = readCookie("splash");        
+        if (ckie == null && are_cookies_enabled()==true  ){ //Check to see if a cookie with name of "query" exists
+            window.location.replace(Drupal.settings.splash_screen.splash);
+            createCookie("splash","splash",365);
+        }
+        else if(are_cookies_enabled()==true && window.location==Drupal.settings.splash_screen.base_url)
+            window.location.replace(Drupal.settings.splash_screen.base_url+"/"+readCookie(Drupal.settings.splash_screen.cookie_name));
+        
+        $('ul.languages li a').click(function() {
+            $lg = $(this).find("span").html();
 
-	  $.cookie("ecweb_cookie_language", $lg, { expires: 365 });
+            $.cookie(Drupal.settings.splash_screen.cookie_name, $lg, {
+                expires: 365
+            });
 	  
-      return true;
-    });
+            return true;
+        });
 	  	
-  });
+    });
 })(jQuery);
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function are_cookies_enabled()
+{
+	var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+	if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled)
+	{
+		document.cookie="testcookie";
+		cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+	}
+	return (cookieEnabled);
+}
+
