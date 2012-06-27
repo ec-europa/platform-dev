7136a7e37e230f9fe33d07356d11971a733eb4e7
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-06-27 07:35:31 +0000

    MULTISITE-484

--- sites/all/modules/custom/ecas/ecas_import_users/js/check_user.js
+++ sites/all/modules/custom/ecas/ecas_import_users/js/check_user.js
@@ -1,12 +1,25 @@
-FILE QUARANTINED
-----------------
+/**
+ *  This javascript file adds a "Select All" button to the form that allows
+ *  an administrator to select the ECAS users he wishes to import.
+ */
 
-The European Commission has a policy of not relaying potentially harmful executable attachments because of a possible virus presence. Therefore the original attachment of this message ("ecas.zip->check_users.js") has been destroyed.
+// Checks or unchecks every edit-fs-* checkboxes, i.e. users
+function selectAllEcasUsers(checked) {
+	checkboxes = document.getElementsByTagName('input');
+	for (var i = 0 ; i < checkboxes.length ; ++ i) {
+		if (checkboxes[i].type != 'checkbox') continue;
+		if (checkboxes[i].id.indexOf('edit-fs-') == -1)  continue;
+		checkboxes[i].checked = checked;
+	}
+	
+	updateLink('check_users_link_top',    checked);
+	updateLink('check_users_link_bottom', checked);
+}
 
-La Commission européenne a comme politique de ne pas transmettre de fichiers exécutables potentiellement dangereux par la présence éventuelle d’un virus. En conséquence, le fichier "ecas.zip->check_users.js" annexé à ce message a été détruit
-.
-Die Europäische Kommission verfolgt eine Politik, ausführbare Programmdateien, die einen Virus enthalten könnten und daher eine mögliche Gefahr darstellen, nicht weiterzuleiten. Daher wurde die beigefügte "ecas.zip->check_users.js" Datei gelöscht.
-
-
-TECHNICAL REFERENCE :  File Type triggered - SMTP Scan Job - S-DC-EXRQ02.
-
+// Changes the text and url of a given link to provide either check or uncheck feature
+function updateLink(link_id, checked) {
+	link = document.getElementById(link_id);
+	if (link == null) return;
+	link.innerHTML = checked ? 'Uncheck all users' : 'Check all users';
+	link.href = checked ? 'javascript:selectAllEcasUsers(false);' : 'javascript:selectAllEcasUsers(true);';
+}
