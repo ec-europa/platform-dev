85cdaaa2e2dfeb29f3bcf88b7e35f8dd811ee218
degliwe <degliwe@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-07-03 11:11:02 +0000

    MULTISITE-540

--- sites/all/themes/ec_default/templates/block--multisite-og-button--og-contextual-links.tpl.php
+++ sites/all/themes/ec_default/templates/block--multisite-og-button--og-contextual-links.tpl.php
@@ -46,7 +46,7 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="btn-group"<?php print $content_attributes; ?>>
-	  <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle"><? print t('Create content'); ?><span class="caret"></span></a>
+	  <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle"><?php print t('Create content'); ?> <span class="caret"></span></a>
 		<?php print $content ?>
    </div><!-- /btn-group -->  
   
