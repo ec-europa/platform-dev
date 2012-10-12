964e3312fec384c8d7faa516bdf866e60ac3bc63
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-10-12 13:52:43 +0000

    MULTISITE-791

--- profiles/multisite_drupal_core/inject_data.php
+++ profiles/multisite_drupal_core/inject_data.php
@@ -80,12 +80,5 @@ function inject_data() {
     echo "Node saved!\n";
   }  
   
-  // change article content type description
-  db_update('node_type')
-    ->fields(array('description'=>'Use articles for time-sensitive content.'))
-    ->condition('type', 'article')
-    ->execute();
-  node_type_cache_reset();
-   
 }
 
--- profiles/multisite_drupal_standard/inject_data.php
+++ profiles/multisite_drupal_standard/inject_data.php
@@ -108,13 +108,6 @@ function inject_data() {
     echo "Node saved!\n";
   }  
   
-  // change article content type description
-  db_update('node_type')
-    ->fields(array('description'=>'Use articles for time-sensitive content.'))
-    ->condition('type', 'article')
-    ->execute();
-  node_type_cache_reset();
-  
 }
 
 
