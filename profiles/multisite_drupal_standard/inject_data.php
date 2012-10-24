4f43ce66460dbbf83ab2f312b8bef3e2ba10d009
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-10-24 14:48:25 +0000

    MULTISITE-885

--- profiles/multisite_drupal_standard/inject_data.php
+++ profiles/multisite_drupal_standard/inject_data.php
@@ -110,7 +110,9 @@ function inject_data() {
  
   //delete mails from the update manager module
   variable_del("update_notify_emails");
-  
+ 
+  // clear links from the linkchecker scanning
+  drush_linkchecker_clear();
 }
 
 
