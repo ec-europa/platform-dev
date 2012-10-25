7e64c73481c23d9510b20448440e2e011f777813
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2012-10-25 08:05:33 +0000

    MULTISITE-877

--- sites/default/proxy.settings.php
+++ sites/default/proxy.settings.php
@@ -7,4 +7,4 @@ $conf['proxy_port'] = 8012;
 $conf['proxy_username'] = 'j50l033';
 $conf['proxy_password'] = 'cU2M>!:7';
 $conf['proxy_user_agent'] = 'Drupal Multisite (msp0)';
-$conf['proxy_exceptions'] = array(php_uname('n'), 'fpfis-dev', '127.0.0.1', 'localhost', 'biguonia.cc.cec.eu.int', '158.167.39.277', 'dbprod-dmrz.jrc.org', '139.191.254.129','intragate.acceptance.ec.europa.eu');
+$conf['proxy_exceptions'] = array(php_uname('n'), 'ec.europa.eu', 'fpfis-dev', '127.0.0.1', 'localhost', 'biguonia.cc.cec.eu.int', '158.167.39.277', 'dbprod-dmrz.jrc.org', '139.191.254.129','intragate.acceptance.ec.europa.eu');
