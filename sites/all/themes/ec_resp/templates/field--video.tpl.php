17f7811ced4c1b0b89dd05107194893fcb67e0dd
farcaov <farcaov@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-03-15 12:55:03 +0000

    MULTISITE-1522 - Subtask 8 - Refactored the feature video

--- sites/all/themes/ec_resp/templates/field--video.tpl.php
+++ sites/all/themes/ec_resp/templates/field--video.tpl.php
@@ -40,8 +40,6 @@
  * @see template_preprocess_field()
  * @see theme_field()
  */
-global $base_url, $theme;
-$themepath = drupal_get_path('theme', $theme);
 ?>
 <!--
 THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
@@ -50,82 +48,5 @@ After copying this file to your theme's folder and customizing it, remove this
 HTML comment.
 -->
 <div class="media_gallery row-fluid">
-  <ul class="thumbnails">
-
-    <?php foreach ($items as $delta => $item): ?>
-
-      <?php
-      $converted_fid = db_select('video_output', 'vd')
-              ->fields('vd', array('output_fid'))
-              ->condition('original_fid', $item['#item']['fid'], '=')
-              ->execute()
-              ->fetchAssoc();
-
-      if (isset($converted_fid) && $converted_fid != NULL) {
-        $converted_video = file_load($converted_fid['output_fid']); //very useful object
-
-        $video_path = $base_url . '/' . file_stream_wrapper_get_instance_by_uri('public://')->getDirectoryPath() . str_replace('public://', '/', $converted_video->uri);
-      } else {
-        $video_path = $base_url . '/' . file_stream_wrapper_get_instance_by_uri('public://')->getDirectoryPath() . '/videos/original/' . $item['#item']['filename'];
-      }
-
-
-      //REPLACE $localdata with $item['#item']
-      $thumb = file_load($item['#item']['thumbnail']);
-
-      if (!empty($thumb)) {
-        $video_square_thumbnail = image_style_url('square_thumbnail', $thumb->uri);
-        $video_preview = image_style_url('preview', $thumb->uri);
-      }
-
-
-
-      $short_name = (strlen(filter_xss($item['#item']['filename'])) > 35) ? substr(filter_xss($item['#item']['filename']), 0, 30) . '[...]' : filter_xss($item['#item']['filename']);
-      ?>
-
-
-       <?php
-//activate it if necessary
-//print render($item);
-      ?>
-        <li class="span3">
-          <!-- $key replace by $element['#field_name']-->
-          <div id="video_lightbox<?php print $delta; ?>" class="lightbox" style="display: none;">
-
-
-            <object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='800' height='600' id='<?php print $element['#field_name']; ?>' name='<?php print $element['#field_name']; ?>'>
-              <param name='movie' value='http://ec.europa.eu/wel/players/jwflvplayer/player.swf'>
-              <param name='allowfullscreen' value='true'>
-              <param name='allowscriptaccess' value='always'>
-              <param name='flashvars' value='file=<?php print $video_path; ?>&fullscreen=true&image=<?php print $video_preview; ?>&skin=http://ec.europa.eu/wel/players/jwflvplayer/skins/mainley.swf'>
-              <!--<param name='flashvars' value='file=playlist.xml'>
-              inside embed will be
-              flashvars="file=playlist.xml"
-              -->
-              <embed id='<?php print $element['#field_name']; ?>'
-                     name='<?php print $element['#field_name']; ?>'
-                     src='http://ec.europa.eu/wel/players/jwflvplayer/player.swf'
-                     width='800'
-                     height='600'
-                     allowscriptaccess='always'
-                     allowfullscreen='true'
-                     flashvars="file=<?php print $video_path; ?>&fullscreen=true&image=<?php print $video_preview; ?>&skin=http://ec.europa.eu/wel/players/jwflvplayer/skins/mainley.swf"
-                     />
-            </object>
-
-          </div>
-
-          <a href="#video_lightbox<?php print $delta; ?>" class="fancybox thumbnail" rel="gallery" title="<?php $item['#item']['filename']; ?>">
-            <img class="watermark" src="<?php print $base_url . '/' . $themepath; ?>/images/video_icon.png" alt="<?php $item['#item']['filename']; ?>" title="<?php $item['#item']['filename']; ?>" />
-            <img src="<?php print $video_square_thumbnail; ?>" alt="<?php $item['#item']['filename']; ?>" title="<?php $item['#item']['filename']; ?>" />
-            <p class="carousel-caption"><?php print $short_name; ?></p>
-          </a>
-
-
-
-        </li>
-
-<?php endforeach; ?>
-
-  </ul>
+  <?php print render($videos); ?>
 </div>
