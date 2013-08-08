8c1dcfd94fcfb27b16f986d94785cccc80de2bd2
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-08-08 10:18:38 +0000

    MULTISITE-2115 Add sub theme skeleton

---
+++ sites/all/themes/sub_theme/scripts/sub_theme.js
@@ -0,0 +1,17 @@
+/*
+ * custom javascript for the sub theme
+ */
+
+//use jQuery 1.4.4
+jQuery(function($){
+  $(document).ready(function() {  //Once the page elements are fully loaded
+
+  });
+});
+
+//use jQuery 1.7.1
+(function($){
+  $(document).ready(function() {  //Once the page elements are fully loaded
+
+  });
+});  
