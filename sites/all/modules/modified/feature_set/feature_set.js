f77fb4785e004bfc1bbfd023d4938518ba2d57dd
farcaov <farcaov@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-08-20 08:05:48 +0000

    MULTISITE-2030 Feature Set 1.1 (simpletest to be tested)

--- sites/all/modules/modified/feature_set/feature_set.js
+++ sites/all/modules/modified/feature_set/feature_set.js
@@ -15,21 +15,13 @@ jQuery(function($){
       .closest('tr')
       .addClass('form-disabled');
   
-    //Add switcher before checkbox and hide checkbox
-    /*$("#feature-set-admin-form .form-checkbox")
-      .css('opacity',0)
-      .filter(':checked')
-        .before('<label class="cb-enable selected"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable"><span><i class="icon-remove icon-white"></i></span></label>')
-      .end()
-      .not(':checked')
-        .before('<label class="cb-enable"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable selected"><span><i class="icon-remove icon-white"></i></span></label>');
-    */
     $("#feature-set-admin-form .form-checkbox").each(function() {
       $this = $(this);
       //Check if the feature has been enabled
       if ($this.is(':checked')) {
         var html_before = '<label class="cb-enable selected"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable"><span><i class="icon-remove icon-white"></i></span></label>';
-      } else {
+      }
+      else {
         var html_before = '<label class="cb-enable"><span><i class="icon-ok icon-white"></i></span></label><label class="cb-disable selected"><span><i class="icon-remove icon-white"></i></span></label>';
       }     
       
@@ -42,20 +34,21 @@ jQuery(function($){
     $('#feature-set-admin-form tr').click(function() {
       $(this)
         .not('.form-disabled')
-          .find('.switch')
-            .toggleClass('pending')
-            .children('label')
-              .toggleClass('selected')
-            .end()
-            .children('.form-checkbox')
-              .each(function() {
-                $this = $(this);
-                if ($this.is(':checked')) {
-                  $this.attr('checked', false);
-                } else {
-                  $this.attr('checked', true);
-                }               
-              }); 
+        .find('.switch')
+        .toggleClass('pending')
+        .children('label')
+        .toggleClass('selected')
+        .end()
+        .children('.form-checkbox')
+        .each(function() {
+          $this = $(this);
+          if ($this.is(':checked')) {
+            $this.attr('checked', false);
+          }
+          else {
+            $this.attr('checked', true);
+          }               
+        }); 
     });
   });
 });
