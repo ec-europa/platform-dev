377fd6abf76e030fcccc0b8e5cc4523bfe7c10e4
farcaov <farcaov@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-10-22 14:42:53 +0000

    MULTISITE-2311 - remove simplenews add content

--- sites/all/themes/ec_resp/templates/simplenews-multi-block.tpl.php
+++ sites/all/themes/ec_resp/templates/simplenews-multi-block.tpl.php
@@ -16,28 +16,6 @@
  * @see template_preprocess_simplenews_multi_block()
  */
 ?>
-
-	<?php 
-	//check user permissions
-	$add_newsletter = (user_access('administer nodes') ? 1 : 0);
-	$add_category = (user_access('administer taxonomy') ? 1 : 0);
-
-	$attributes_first = array('type' => 'add', 'action_bar' => 'first', 'btn_group' => 'first');
-	$attributes_last = array('type' => 'add', 'action_bar' => 'last', 'btn_group' => 'last');
-	$attributes_single = array('type' => 'add', 'action_bar' => 'single', 'btn_group' => 'single');
-
-	if ($add_newsletter && $add_category) {
-		print l(t('Create a Newsletter issue'), 'node/add/simplenews', array('attributes' => $attributes_first));
-		print l(t('Create a Newsletter'), 'admin/config/services/simplenews', array('attributes' => $attributes_last));
-	} 
-	else if ($add_newsletter) {
-		print l(t('Create a Newsletter issue'), 'node/add/simplenews', array('attributes' => $attributes_single));
-	}
-	else if ($add_category) {
-		print l(t('Create a Newsletter'), 'admin/config/services/simplenews', array('attributes' => $attributes_single));
-	}
-	?>
-
   <?php if ($message): ?>
     <p><?php print $message; ?></p>
   <?php endif; ?>
