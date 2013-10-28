39091fec6eef939a01bb7dba57fd364c7b495d20
leperde <leperde@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-10-28 14:29:11 +0000

    MULTISITE-2374 - fix file format

--- dev/jenkins/bootstrap.sh
+++ dev/jenkins/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 if [ ! -e "composer.phar" ]; then
-    curl -s https://getcomposer.org/installer | php
+    curl -sS https://getcomposer.org/installer | php
 fi
 
 php composer.phar install
