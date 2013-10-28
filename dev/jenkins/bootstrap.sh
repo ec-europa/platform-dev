a3f43d01a28e9882ed58086b186c19d04a085f13
leperde <leperde@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-10-28 13:44:37 +0000

    MULTISITE-2374 - Add bootstrap script

---
+++ dev/jenkins/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -e "composer.phar" ]; then
+    curl -sS https://getcomposer.org/installer | php
+fi
+
+php composer.phar install
+
+if [ ! -e "behat.yml" ]; then
+    cp behat.yml.dist behat.yml
+    echo "[Behat] You have to fill your behat.yml configuration file."
+    exit 1
+fi
+
+if [ ! -e "build.properties" ]; then
+    cp build.default.properties build.properties
+    echo "[Phing] You have to fill your build.properties configuration file."
+    exit 1
+fi
