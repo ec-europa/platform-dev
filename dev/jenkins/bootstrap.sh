326116ab88ddbb9d597bdfdcef629c23c62bef2f
leperde <leperde@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-10-28 15:15:58 +0000

    MULTISITE-2374 - Permissions

--- dev/jenkins/bootstrap.sh
+++ dev/jenkins/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -e "composer.phar" ]; then
-    curl -s https://getcomposer.org/installer | php
+    curl -sS https://getcomposer.org/installer | php
 fi
 
 php composer.phar install
