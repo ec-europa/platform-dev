e3d419825724730fb3f021a13436820aaf8fb6e4
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-11-05 14:04:27 +0000

    PMOCONTACT-346 connexion between multisite and comref

--- sites/all/modules/custom/multisite_oracle_access/server_side_sample.php
+++ sites/all/modules/custom/multisite_oracle_access/server_side_sample.php
@@ -2,31 +2,42 @@
 /*
  * sample code for comref database
  */
+
+$settings_file = dirname(__FILE__) . '/settings_oracle.php';
+
+if (file_exists($settings_file)) {
+  require_once($settings_file);
   
-$xml = new DOMDocument('1.0', 'utf-8');
-$params = $xml->createElement('data');
-$xml->appendChild($params);
+  if(isset($_POST['access_token']) && $_POST['access_token'] == $access_token) {
+    $xml = new DOMDocument('1.0', 'utf-8');
+    $params = $xml->createElement('data');
+    $xml->appendChild($params);
 
-$fields = explode(',', $_POST['fields']);
+    $tns_admin_path = dirname(realpath(__FILE__)) . '/oracle_home';
+    putenv('TNS_ADMIN=' . $tns_admin_path);
+    $connection_string = 'XXXX';
+    $connection_handle = oci_connect($username, $passwd, $connection_string);
+    if (!$connection_handle) {
+      print "Unable to connect: " . print_r(oci_error(), TRUE);
+    }
 
-$tns_admin_path = dirname(realpath(__FILE__)) . '/oracle_home';
-putenv('TNS_ADMIN=' . $tns_admin_path);
-$connection_string = 'XXXX';
-$connection_handle = oci_connect('XXXXX', 'XXXXX', $connection_string);
-if (!$connection_handle) {
-  print "Unable to connect: " . print_r(oci_error(), TRUE);
-}
+    $statement = oci_parse($connection_handle, 'select * from APP_CRF_P.CRF4PMOCNT_NUPS_V WHERE PER_ID = :perid');
+    oci_bind_by_name($statement, ":perid", $_POST['per_id']);
+    oci_execute($statement);
 
-$per_id = $_POST['per_id'];
-
-$statement = oci_parse($connection_handle, 'select * from APP_CRF_P.CRF4PMOCNT_NUPS_V WHERE PER_ID = :perid');
-oci_bind_by_name($statement, ":perid", $per_id);
-oci_execute($statement);
-
-while ($row = oci_fetch_array($statement, OCI_ASSOC)) {
-  foreach ($fields as $item) {
-    $param = $xml->createElement($item, $row[strtoupper($item)]);
-    $params->appendChild($param);
+    $fields = explode(',', $_POST['fields']); // get fields to get from the Oracle view
+    while ($row = oci_fetch_array($statement, OCI_ASSOC)) {
+      foreach ($fields as $item) {
+        $param = $xml->createElement($item, $row[strtoupper($item)]);
+        $params->appendChild($param);
+      }
+    }
+    echo $xml->saveXML();
   }
+  else {
+    print "acces denied to service";
+  }  
 }
-echo $xml->saveXML();
+else {
+  print "settings not found";   
+}
---
+++ sites/all/modules/custom/multisite_oracle_access/settings_oracle.php
@@ -0,0 +1,5 @@
+<?php
+$username = 'XXX';
+$passwd = 'XXX';
+$connection_string = 'XXX';
+$access_token = 'XXX';
