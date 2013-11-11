878921dd322a42a1a0b464ad5b01036e9161540e
champcy <champcy@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2013-11-11 09:35:58 +0000

    PMOCONTACT-346 connexion between multisite and comref

--- sites/all/modules/custom/multisite_oracle_access/server_side_sample.php
+++ sites/all/modules/custom/multisite_oracle_access/server_side_sample.php
@@ -20,7 +20,7 @@ if (file_exists($settings_file)) {
       print "Unable to connect: " . print_r(oci_error(), TRUE);
     }
 
-    $statement = oci_parse($connection_handle, 'select * from APP_CRF_P.CRF4PMOCNT_NUPS_V WHERE PER_ID = :perid');
+    $statement = oci_parse($connection_handle, 'select * from APP_CRF_P.CRF4PMOCNT_NUPS_V WHERE PER_ID = :perid ORDER BY DT_ATTRIB_LIEN DESC');
     oci_bind_by_name($statement, ":perid", $_POST['per_id']);
     oci_execute($statement);
 
