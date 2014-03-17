ad280efde6bb9ab15e8537c20c088f41bcd2cd73
leperde <leperde@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2014-03-17 10:50:52 +0000

    MULTISITE-3365 remove the blocking step in sh script

--- dev/jenkins/bootstrap.sh
+++ dev/jenkins/bootstrap.sh
@@ -6,12 +6,6 @@ fi
 
 php composer.phar install
 
-if [ ! -e "behat.yml" ]; then
-    cp behat.yml.dist behat.yml
-    echo "[Behat] You have to fill your behat.yml configuration file."
-    exit 1
-fi
-
 if [ ! -e "build.properties" ]; then
     cp build.default.properties build.properties
     echo "[Phing] You have to fill your build.properties configuration file."
