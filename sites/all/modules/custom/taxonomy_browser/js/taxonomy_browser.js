fd8a1ff461fd416d03e549fec6bc72dce50148bf
emguncikr <emguncikr@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2014-04-09 15:27:27 +0000

    MULTISITE-3299 replacing the jstree library with custom js, adding some basic styles, adding opener in the template file

--- sites/all/modules/custom/taxonomy_browser/js/taxonomy_browser.js
+++ sites/all/modules/custom/taxonomy_browser/js/taxonomy_browser.js
@@ -1,20 +1,31 @@
 /**
  * @file
- * create a jstree object for the taxonomy_browser module
+ * create a dropdown
  *  
  */
 
 (function($){
-$(document).ready(function(){
-	$(".tb_browser_tree").jstree({ 
-		"themes" : {
-			"theme" : "ec-default",
-			"dots" : true,
-			"icons" : true
-		},
-		"cookies": { "tb_browser_tree": { "path": "/"} }, 
-		"plugins" : [ "themes", "html_data", "cookies" ],
-	});
+	Drupal.behaviors.taxonomy_browser = {
+		attach: function(context, settings) {
+			$('.tb-browser-tree').once('tb-browser-tree', function(){
+				$('.tb-browser-tree > ul.taxonomy-tree').find('ul.taxonomy-tree').hide();
+				var $currentActive = $(this).find('a.active');
 
-});
+				$currentActive.parents('ul.taxonomy-tree').show().siblings('.taxonomy-tree-opener').removeClass('glyphicon-plus').addClass('glyphicon-minus');;
+				$('.taxonomy-tree-opener').click(function(e){
+					e.preventDefault();
+
+					if ($(this).hasClass('glyphicon-minus')) {
+						$(this).removeClass('glyphicon-minus');
+						$(this).addClass('glyphicon-plus');
+					} else {
+						$(this).addClass('glyphicon-minus');
+						$(this).removeClass('glyphicon-plus');
+					}
+
+					$(this).siblings('ul.taxonomy-tree').stop(true, true).slideToggle(200);
+				});
+			});
+		}
+	}
 })(jQuery);
