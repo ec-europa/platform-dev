7d2853087d7d307efd56146ea4682b0667fccdac
guerrxa <guerrxa@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2014-11-10 10:37:02 +0000

    MULTISITE-4636: add new scripts to crawl eacea-site.

---
+++ crawling_scripts/http-eacea.ec.europa.eu-about-de.conf.js
@@ -0,0 +1,15 @@
+ec_site_name = 'EACEA site';
+ec_protocol = 'http';
+ec_domain = 'eacea.ec.europa.eu';
+ec_base_path = '/uber-eacea';
+ec_base_url = ec_protocol + '://' + ec_domain + ec_base_path;
+ec_start_urls = [ec_base_url + '_de'];
+
+ec_save_url_filepath  = './state/' + ec_protocol + "-" + ec_domain + "-";
+ec_save_url_filepath += ec_base_path.replace(/^\//, "").replace(/\//g, "-") + ".urls.json";
+
+// Cookie header (which is systematically forced)
+ec_cookie_header = 'language=en; has_js=1';
+
+// do not skip external resources for eacea because of its particular set of mappings
+skip_external_resources = false;
