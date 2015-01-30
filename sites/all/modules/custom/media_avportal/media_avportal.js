cabe9dc686aec74889b72295d8ba97e7a38dee21
lemaith <lemaith@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2015-01-30 15:55:28 +0000

    MULTISITE-5076 click fixed + iframe can be or not embedded into p tag

--- sites/all/modules/custom/media_avportal/media_avportal.js
+++ sites/all/modules/custom/media_avportal/media_avportal.js
@@ -6,12 +6,15 @@
     attach : function(context, settings) {
 
       $('div.language-switcher li a').click(
-        function() {
-
+        function(e) {
+          e.preventDefault();
           $(this).parents('ul').find('a.current').removeClass('current');
           $(this).addClass('current');
           var iframe = $(this).parents('div.language-switcher').siblings('p')
                 .find('iframe');
+          if(!iframe.length) {
+            iframe = $(this).parents('div.language-switcher').siblings('iframe')
+          }
           var lng = $(this).text().replace('+', ' ');
           var new_src = iframe.attr('src').replace(
                 new RegExp("videolang=(.+)", "g"), 'videolang=' + lng);
