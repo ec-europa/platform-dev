1c370810a555584d8b21f44f8baf0363b2b528c2
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2015-03-09 14:18:14 +0000

    NEXTEUROPA-2332 Implement new version of splash screen

--- sites/all/modules/features/custom/splash_screen/theme/splash.tpl.php
+++ sites/all/modules/features/custom/splash_screen/theme/splash.tpl.php
@@ -1,41 +1,23 @@
 <?php
+
 /**
- * @file splash.tpl.php
- *
+ * @file
  * Default implementation of the splash page content.
  *
  * Available variables:
- *
  * - $site_name: the site name.
  * - $languages_list: the language list.
+ * - $languages_list_array: unformated array of languages. 
+ * - $languages_blacklist: unformated array of blacklisted language codes.
  * - $base_url: the base_url of the site instance.
  *
  * @see template_preprocess()
- * @see template_preprocess_page()
+ * @see template_preprocess_splash()
  * @see template_process()
- * @see ec_default_process_page()
  */
 ?>
 
-<a id="top-page" name="top-page"></a>
-
-<div class="splash container-fluid">
-  <div class="row">
-    <!-- picture and title -->
-    <div class="col-lg-3 offset2 center">
-      <div id="logo"></div>
-      <h1><?php print $site_name; ?></h1>
-    </div>
-
-    <!-- language list -->
-    <div class="col-lg-5">
-      <ul class="well languages">
-        <li><h2 id="label_language"><?php print t('Please choose a language'); ?></h2></li>
-        <?php print $languages_list; ?>
-      </ul>
-    </div>
-  </div>
-</div>
-<script type="text/javascript">
-  var templatePath = "<?php print $base_url . '/' . path_to_theme(); ?>";
-</script>
+<nav class="site-level-language-selector">
+  <h1 class="icon-language"><?php print t('Select language'); ?>:</h1>
+  <?php print $languages_list; ?>
+</nav>
