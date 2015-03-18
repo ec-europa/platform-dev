6e4950f4234a21be3d4fcf629378de25a5329c12
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2015-03-18 14:04:19 +0000

    NEXTEUROPA-2335 Check if links are internal or external

--- sites/all/modules/features/custom/nexteuropa_multilingual/nexteuropa_multilingual.js
+++ sites/all/modules/features/custom/nexteuropa_multilingual/nexteuropa_multilingual.js
@@ -13,28 +13,41 @@
       if (secondLanguageValue) {
         addSecondLanguage(secondLanguageParam, secondLanguageValue);
       }
-      
+
       /**
        * Add second preferred language to every link.
        */
       function addSecondLanguage(paramName, paramValue) {
-        $('a[href]').each(function(){
-          if (!getParameter(paramName, this.href)) {
+        $('a[href]').each(function() {
+          if (!getParameter(paramName, this.href) && isCurrentDomain(this.href)) {
             var sep = (this.href.indexOf('?') != -1) ? '&' : '?'; 
              $(this).attr('href', this.href + sep + paramName + '=' + paramValue);
           }
         });
-        $('form').each(function(){
-          if (!getParameter(paramName, this.action)) {
-            var sep = (this.action.indexOf('?') != -1) ? '&' : '?'; 
+        $('form').each(function() {
+          if (!getParameter(paramName, this.action) && isCurrentDomain(this.action)) {
+            var sep = (this.action.indexOf('?') != -1) ? '&' : '?';
             $(this).attr('action', this.action + sep + paramName + '=' + paramValue);
           }
         });
       }
 
       /**
+       * Check if link leads to current domain or not.
+       * Returns true if link leads to current domain, and false if not.
+       */
+      function isCurrentDomain(linkUrl) {
+        if (linkUrl.indexOf(document.domain) == -1) {
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+
+      /**
        * Helper function to get parameter.
-       * Return parameter value, or false if not found.
+       * Returns parameter value, or false if not found.
        */
       function getParameter(paramName, location) {
         var params = location.split('?');
