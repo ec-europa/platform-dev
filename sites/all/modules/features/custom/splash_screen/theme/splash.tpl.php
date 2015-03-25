87125c911a65fab6255b77347f631252c37ad19f
emeryro <emeryro@e4cdaa0f-fb2f-4b60-885c-ce0c5f15d557>
2015-03-25 12:52:40 +0000

    NEXTEUROPA-2578 Add language selector (site level)

--- sites/all/modules/features/custom/splash_screen/theme/splash.tpl.php
+++ sites/all/modules/features/custom/splash_screen/theme/splash.tpl.php
@@ -5,11 +5,10 @@
  * Default implementation of the splash page content.
  *
  * Available variables:
- * - $site_name: the site name.
+ * - $close_button: unformatted close button.
  * - $languages_list: the language list.
  * - $languages_list_array: unformated array of languages. 
  * - $languages_blacklist: unformated array of blacklisted language codes.
- * - $base_url: the base_url of the site instance.
  *
  * @see template_preprocess()
  * @see template_preprocess_splash()
@@ -20,4 +19,5 @@
 <nav class="site-level-language-selector">
   <img src="<?php print drupal_get_path('module', 'splash_screen'); ?>/theme/icon_language.png" width="64" height="64" alt="Select language" title="Select language" />
   <?php print $languages_list; ?>
+  <?php print $close_button; ?>
 </nav>
