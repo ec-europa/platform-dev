783b41b08c23f5fbdf2c7f2db17849b8f1068671
EMERY Romain <romain.emery@ext.ec.europa.eu>
2015-05-22 08:27:15 +0200

    MULTISITE-6145 Check variable

--- profiles/common/themes/ec_resp/templates/global/html.tpl.php
+++ profiles/common/themes/ec_resp/templates/global/html.tpl.php
@@ -4,7 +4,7 @@
  * Default theme implementation of main page.
  */
 ?><!DOCTYPE html>
-<html lang="<?php print $language->language; ?>">
+<html lang="<?php if (isset($language)) print $language->language; ?>">
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <?php print $head; ?>
