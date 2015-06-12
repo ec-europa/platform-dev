a3a721031546846a9dcc7e38f80877ce3cb970d5
DE MARCO Antonio <antonio.demarco@engib.be>
2015-06-12 10:54:43 +0200

    NEXTEUROPA-3763: Start working on field api tests.

---
+++ profiles/common/modules/custom/multisite_config/tests/field/ConfigTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\multisite_config\Tests\NextEuropaDataExportTest.
+ */
+
+namespace Drupal\multisite_config\Tests\field;
+
+use Drupal\multisite_config\Tests\MultisiteConfigAbstractTest;
+use Drupal\field\Config;
+
+/**
+ * Class ConfigTest.
+ *
+ * @package Drupal\multisite_config\Tests\field
+ */
+class ConfigTest extends MultisiteConfigAbstractTest {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmokeTest() {
+
+    $this->assertTrue(TRUE);
+  }
+
+}
