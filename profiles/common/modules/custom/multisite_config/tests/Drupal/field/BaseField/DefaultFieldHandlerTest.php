3295ff3c69eee8524e9088e31d526695d94579c9
DE MARCO Antonio <antonio.demarco@engib.be>
2015-06-18 11:56:35 +0200

    NEXTEUROPA-3763: Adding basic field handling tests.

---
+++ profiles/common/modules/custom/multisite_config/tests/Drupal/field/BaseField/DefaultFieldHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains \Tests\Drupal\field\BaseField\DefaultFieldHandler.
+ */
+
+namespace Drupal\multisite_config\Tests\Drupal\field\BaseField;
+
+use Drupal\multisite_config\Tests\ConfigAbstractTest;
+use Drupal\field\BaseField\DefaultFieldHandler;
+
+/**
+ * Class DefaultFieldHandlerTest.
+ *
+ * @package Drupal\multisite_config\Tests\Drupal\field
+ */
+class DefaultFieldHandlerTest extends ConfigAbstractTest {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmokeTest() {
+    $this->assertTrue(TRUE);
+  }
+
+}
---
+++ profiles/common/modules/custom/multisite_config/tests/Drupal/field/InstanceField/DefaultFieldHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains \Tests\Drupal\field\InstanceField\DefaultFieldHandler.
+ */
+
+namespace Drupal\multisite_config\Tests\Drupal\field\InstanceField;
+
+use Drupal\multisite_config\Tests\ConfigAbstractTest;
+use Drupal\field\InstanceField\DefaultFieldHandler;
+
+/**
+ * Class DefaultFieldHandlerTest.
+ *
+ * @package Drupal\multisite_config\Tests\Drupal\field\InstanceField
+ */
+class DefaultFieldHandlerTest extends ConfigAbstractTest {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmokeTest() {
+    $this->assertTrue(TRUE);
+  }
+
+}
