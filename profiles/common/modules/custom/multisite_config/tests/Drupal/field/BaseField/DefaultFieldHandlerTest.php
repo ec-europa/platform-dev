57183430940a37c2fdd23d6ab86becbb15bc99fb
DE MARCO Antonio <antonio.demarco@engib.be>
2015-06-18 12:27:28 +0200

    NEXTEUROPA-3763: Adding tests for DefaultFieldHandlerTest.

--- profiles/common/modules/custom/multisite_config/tests/Drupal/field/BaseField/DefaultFieldHandlerTest.php
+++ profiles/common/modules/custom/multisite_config/tests/Drupal/field/BaseField/DefaultFieldHandlerTest.php
@@ -13,15 +13,46 @@ use Drupal\field\BaseField\DefaultFieldHandler;
 /**
  * Class DefaultFieldHandlerTest.
  *
- * @package Drupal\multisite_config\Tests\Drupal\field
+ * @package Drupal\multisite_config\Tests\Drupal\field\BaseField
  */
 class DefaultFieldHandlerTest extends ConfigAbstractTest {
 
   /**
-   * Smoke test.
+   * Test base field handler constructor.
+   *
+   * @dataProvider additionProvider
    */
-  public function testSmokeTest() {
-    $this->assertTrue(TRUE);
+  public function testHandlerConstructor($field_name, $type) {
+    $handler = new DefaultFieldHandler($field_name, $type);
+
+    $this->assertArrayHasKey('field_name', $handler->getField());
+    $this->assertArrayHasKey('type', $handler->getField());
+  }
+
+  /**
+   * Test base field saving.
+   *
+   * @dataProvider additionProvider
+   */
+  public function testFieldSaving($field_name, $type) {
+    $handler = new DefaultFieldHandler($field_name, $type);
+
+    $field = $handler->save();
+    $this->assertEquals($type, $field['type']);
+    $this->assertEquals($field_name, $field['field_name']);
+
+    field_delete_field($field_name);
+  }
+
+  /**
+   * Data provider.
+   *
+   * @see self::testHandlerConstructor()
+   */
+  public function additionProvider() {
+    return array(
+      array('field_name_' . rand(), 'text'),
+    );
   }
 
 }
