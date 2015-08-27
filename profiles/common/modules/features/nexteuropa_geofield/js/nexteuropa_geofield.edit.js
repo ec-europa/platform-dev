897744b3483a8b635e9078d9afcebcb8b13ed2c3
CHAMPAGNE Cyril <cyril.champagne@ext.ec.europa.eu>
2015-08-27 17:58:47 +0200

    NEXTEUROPA-5213-implement-a-module-to-manage

--- profiles/common/modules/features/nexteuropa_geofield/js/nexteuropa_geofield.edit.js
+++ profiles/common/modules/features/nexteuropa_geofield/js/nexteuropa_geofield.edit.js
@@ -21,14 +21,14 @@
       loadedMap = jQuery.parseJSON(settings.nexteuropa_geojson.map);
       drawnItems = L.geoJson(loadedMap).addTo(map);
 
-      i = 0;
       for(key in drawnItems._layers) {
         // Create popups.
         drawnItems._layers[key].bindPopup(drawnItems._layers[key].feature.properties.label);
-        if(settings.nexteuropa_geojson.settings.fs_objects.prepopulate_label.prepopulate == 0)
+        // Only add inputs elements if the popups are not pre-populated.
+        if(settings.nexteuropa_geojson.settings.fs_objects.prepopulate_label.prepopulate == 0) {
           // Create forms elements to manage popups content.
           create_label(key, drawnItems._layers[key].feature.properties.label);
-        i++;
+        }
       }
       // Focus on map elements.
       map.fitBounds(drawnItems.getBounds());
@@ -77,7 +77,7 @@
 
         // Update GeoJSON field.
         geojson_map = drawnItems.toGeoJSON();
-        $('.field-type-geofield-geojson textarea').text(JSON.stringify(geojson_map));
+        $('#geofield-geojson textarea').text(JSON.stringify(geojson_map));
       }
     });
 
@@ -90,86 +90,67 @@
         objects_count--;
     });
 
+    // Create inputs (and their related events) to manage popups contents.
+    function create_label(leaflet_id, content) {
+      var myinput = '<div id="label_wrapper_'+leaflet_id+'" class="leaflet_label_wrapper"><input class="leaflet_label" name="myField" type="text" id="L'+leaflet_id+'">';
+      myinput = myinput + '<textarea rows="2" class="leaflet_description" id="T'+leaflet_id+'"/>';
+      myinput = myinput + '<a data-label-id="'+leaflet_id+'" class="remove-label" title="Delete labels."><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a></div>';
 
-// Create inputs (and their related events) to manage popups contents.
-function create_label(leaflet_id, content) {
-  var myinput = '<div id="label_wrapper_'+leaflet_id+'" class="leaflet_label_wrapper"><input class="leaflet_label" name="myField" type="text" id="L'+leaflet_id+'">';
-  myinput = myinput + '<a data-label-id="'+leaflet_id+'" class="remove-label" title="Delete labels."><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></div>';
+      $('#geofield_geojson_map_wrapper').append(myinput);
+      $('#L'+leaflet_id).val(content);
 
-  $('#geofield_geojson_map_wrapper').append(myinput);
+      $('#L'+leaflet_id+', #T'+leaflet_id).change(function() {
+        layer = map._layers[leaflet_id];
 
-  $('#L'+leaflet_id).val(content);
+        var popup_content = '<h4>' + $("#L"+leaflet_id).val() + '</h4><p>' + $("#T"+leaflet_id).val() + '</p>';
+  
+        layer.bindPopup(popup_content);
+        layer._popup.setContent(popup_content);
+        layer._popup.update();
+        layer.openPopup();
 
-  $('#L'+leaflet_id).change(function() {
-    layer = map._layers[leaflet_id];
-   
-    layer.bindPopup($("#L"+leaflet_id).val());
-    layer._popup.setContent($("#L"+leaflet_id).val())
-    layer._popup.update();
-    layer.openPopup();
+        // Update GeoJSON field.
+        updateGeoJsonField();
+      });
 
-    // update GeoJSON field
-    updateGeoJsonField();
-    /*
-    geojson_map = drawnItems.toGeoJSON();
-    i = 0
-    for(key in drawnItems._layers) {
-      geojson_map.features[i].properties.label = drawnItems._layers[key]._popup.getContent(); 
-      i++;
+      $('#L'+leaflet_id).focus(function() {
+        layer = map._layers[leaflet_id];
+        layer.openPopup();
+
+        // Focus the map on the map object.
+        if(layer._latlngs)
+          map.setView(layer._latlngs[0]);
+        else
+          map.setView(layer.getLatLng());
+        layer.setStyle({color:'#00FF33'});
+      });
+
+      $('#L'+leaflet_id).blur(function() {
+        layer = map._layers[leaflet_id];
+        layer.closePopup();
+        layer.setStyle({color:'#f06eaa'});
+      }); 
+
+      // Manage the removal of a label.
+      $('.remove-label').click(function() {
+        layer = map._layers[$(this).attr('data-label-id')];
+        layer.closePopup();
+        layer.unbindPopup();
+        $('#L'+leaflet_id).val('');
+      });
     }
-    $('.field-type-geofield-geojson textarea').text(JSON.stringify(geojson_map));
-    */
-  });
 
-  // 
-  $('#L'+leaflet_id).focus(function() {
-   layer = map._layers[leaflet_id];
-   console.log(layer);
-   //layer.setOpacity(1);
-   //layer.togglePopup();
-   layer.openPopup();
+    function updateGeoJsonField() {
+      geojson_map = drawnItems.toGeoJSON();
+      i = 0
+      for(key in drawnItems._layers) {
+        geojson_map.features[i].properties.label = drawnItems._layers[key]._popup.getContent();
+        i++;
+      }
+      $('#geofield-geojson textarea').text(JSON.stringify(geojson_map));
+    }
 
-   if(layer._latlngs)
-     map.setView(layer._latlngs[0]);
-   else
-     map.setView(layer.getLatLng());
-   layer.setStyle({color:'#00FF33'});
-   //layer.setStyle({fillcolor:'#00FF33'});
- });
-
- $('#L'+leaflet_id).blur(function() {
-   layer = map._layers[leaflet_id];
-   //layer.setOpacity(1);
-   //layer.togglePopup();
-   layer.closePopup();
-   layer.setStyle({color:'#f06eaa'});
-   //layer.setOpacity(0.5);
- }); 
-
- $('.remove-label').click(function() {
-   layer = map._layers[$(this).attr('data-label-id')];
-   layer.closePopup();
-   layer.unbindPopup();
-   //$('#label_wrapper_'+leaflet_id).remove();
-   $('#L'+leaflet_id).val('');
- });
-
-}
-
-
-} 
-
-
-function updateGeoJsonField() {
-  geojson_map = drawnItems.toGeoJSON();
-  i = 0
-  for(key in drawnItems._layers) {
-    geojson_map.features[i].properties.label = drawnItems._layers[key]._popup.getContent();
-    i++;
-  }
-  $('.field-type-geofield-geojson textarea').text(JSON.stringify(geojson_map));
-}
-
+  } 
 
 })(jQuery);
 
