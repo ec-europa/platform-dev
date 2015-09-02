5fb15f7b8c0dd8e1b01700f3b132a9f59223006e
CHAMPAGNE Cyril <cyril.champagne@ext.ec.europa.eu>
2015-09-02 16:54:48 +0200

    NEXTEUROPA-5213-implement-a-module-to-manage fix coding standards

--- profiles/common/modules/features/nexteuropa_geofield/js/nexteuropa_geofield.view.js
+++ profiles/common/modules/features/nexteuropa_geofield/js/nexteuropa_geofield.view.js
@@ -4,34 +4,36 @@
  */
 
 (function ($) {
-  Drupal.behaviors.toolbox = {
-    attach: attach
-  };
+    Drupal.behaviors.toolbox = {
+        attach: attach
+    };
 
-  function attach(context, settings) {
+    function attach(context, settings) {
 
-    var map = L.map('geofield_geojson_map', {}).setView([51.505, -0.09], 13);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        var map = L.map('geofield_geojson_map', {}).setView([51.505, -0.09], 13);
+        L.tileLayer(
+            'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }
+      ).addTo(map);
 
-    drawnItems = L.featureGroup().addTo(map);
-    // Load GeoJSON map.
-    if (settings.nexteuropa_geojson.map) {
-      loadedmap = jQuery.parseJSON(settings.nexteuropa_geojson.map);
-      drawnItems = L.geoJson(loadedmap).addTo(map);
+        drawnItems = L.featureGroup().addTo(map);
+        // Load GeoJSON map.
+        if (settings.nexteuropa_geojson.map) {
+            loadedmap = jQuery.parseJSON(settings.nexteuropa_geojson.map);
+            drawnItems = L.geoJson(loadedmap).addTo(map);
 
-      i = 0;
-      for (key in drawnItems._layers) {
-        layer_properties = drawnItems._layers[key].feature.properties;
-        popup_content = buildPopupContent(key, layer_properties.label, layer_properties.description);
-        drawnItems._layers[key].bindPopup(popup_content);
-        i++;
-      }
-      map.fitBounds(drawnItems.getBounds());
-    }
+            i = 0;
+            for (key in drawnItems._layers) {
+                layer_properties = drawnItems._layers[key].feature.properties;
+                popup_content = buildPopupContent(key, layer_properties.label, layer_properties.description);
+                drawnItems._layers[key].bindPopup(popup_content);
+                i++;
+            }
+            map.fitBounds(drawnItems.getBounds());
+        }
 
-    /**
+        /**
      * Build the html content put in a popup.
      * @param {Number} leaflet_id
      *   id of the leaflet layer of the popup
@@ -40,12 +42,12 @@
      * @return {String} description
      *   the content of the popup
      */
-    function buildPopupContent(leaflet_id, name, description) {
-      var content = '<div id="popup_' + leaflet_id + '">';
-      content = content + '<h4 class="popup_name">' + name + '</h4>';
-      content = content + '<p class="popup_description">' + description + '</p>';
-      content = content + '</div>';
-      return content;
+        function buildPopupContent(leaflet_id, name, description) {
+            var content = '<div id="popup_' + leaflet_id + '">';
+            content = content + '<h4 class="popup_name">' + name + '</h4>';
+            content = content + '<p class="popup_description">' + description + '</p>';
+            content = content + '</div>';
+            return content;
+        }
     }
-  }
 })(jQuery);
