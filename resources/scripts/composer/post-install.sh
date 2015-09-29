efe3fe2e2b0525dd3f7dbdd5f2a3c5fa219c39d3
Pieter Frenssen <pieter@frenssen.be>
2015-09-29 18:28:15 +0200

    NEXTEUROPA-5559: Remove existing pre-push hook before creating new one.
    This will automatically update the symlink if it points to another location,
    like it does before this issue is merged in.

--- resources/scripts/composer/post-install.sh
+++ resources/scripts/composer/post-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Symlink the git pre-push hook to its destination.
-if [ ! -h ".git/hooks/pre-push" ] ; then
-  ln -s "../../vendor/pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
+if [ -h ".git/hooks/pre-push" ] ; then
+  rm ".git/hooks/pre-push"
 fi
+ln -s "../../vendor/pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
