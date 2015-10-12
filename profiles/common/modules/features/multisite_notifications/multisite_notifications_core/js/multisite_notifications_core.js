66a091c84757ae26970e8b83a7e538d91190c4ba
SENSER Catherine <catherine.senser@ext.ec.europa.eu>
2015-10-12 14:27:30 +0200

    NEXTEUROPA-6051 : Fix coding standards in multisite_maps, multisite_multilingual_references, multisite_notifications

--- profiles/common/modules/features/multisite_notifications/multisite_notifications_core/js/multisite_notifications_core.js
+++ profiles/common/modules/features/multisite_notifications/multisite_notifications_core/js/multisite_notifications_core.js
@@ -1,8 +1,12 @@
+/**
+ * @file
+ * Notifications javascript file.
+ */
+
 (function ($) {
 
   Drupal.behaviors.multisite_notifications_core = {
     attach: function(context, settings) {
-
 
     }
   };
@@ -10,27 +14,24 @@
 })(jQuery);
 
 
-//use jQuery 1.4.4
-//MOVE HERE ALL THE CODE THAT NEEDS TO BE EXECUTED AS LAST
+// Use jQuery 1.4.4
+// MOVE HERE ALL THE CODE THAT NEEDS TO BE EXECUTED AS LAST.
 jQuery(function($){
-
-  $(document).ready(function() {  //Once the page elements are fully loaded
+  // Once the page elements are fully loaded.
+  $(document).ready(function() {
     var Input = $('#multisite-notifications-core-form input[name=mail]');
     var default_value = Input.val();
 
     $(Input).focus(function() {
-        if($(this).val() == default_value)
-        {
-             $(this).val("");
+        if ($(this).val() == default_value) {
+        $(this).val("");
         }
     }).blur(function(){
-        if($(this).val().length == 0) /*Small update*/
-        {
-            $(this).val(default_value);
+        //Small update
+        if ($(this).val().length == 0) {
+        $(this).val(default_value);
         }
     });
   });
-
-
 
 });
