3b417d3773496b6d3956162f916d878ae3b1aac3
SENSER Catherine <catherine.senser@ext.ec.europa.eu>
2015-10-20 08:23:46 +0200

    NEXTEUROPA-6099 : Interinstitutional theme - beta

--- profiles/common/themes/ec_resp/theme-settings.php
+++ profiles/common/themes/ec_resp/theme-settings.php
@@ -29,4 +29,14 @@ function ec_resp_form_system_theme_settings_alter(&$form, &$form_state) {
     '#title' => t('Enable Interinstitutional theme'),
     '#default_value' => theme_get_setting('enable_interinstitutional_theme'),
   );
+
+  $form['#submit'][] = 'ec_resp_theme_settings_submit';
+}
+
+/**
+ * Form submission handler for ec_resp_form_system_theme_settings_alter().
+ */
+function ec_resp_theme_settings_submit($form, &$form_state) {
+  $separator = ($form_state['values']['enable_interinstitutional_theme'] == 0) ? '>' : ' ';
+  variable_set('easy_breadcrumb-segments_separator', $separator);
 }
