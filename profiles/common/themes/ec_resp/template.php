83376540ea1e13c324460841c59e33b7b9f600fe
Pieter Frenssen <pieter@frenssen.be>
2015-11-11 16:58:38 +0100

    NEXTEUROPA-6997: Fix coding standards.

--- profiles/common/themes/ec_resp/template.php
+++ profiles/common/themes/ec_resp/template.php
@@ -932,8 +932,7 @@ function ec_resp_form_element($variables) {
       '_' => '-',
       '[' => '-',
       ']' => '',
-      )
-    );
+    ));
   }
   // Add a class for disabled elements to facilitate cross-browser styling.
   if (!empty($element['#attributes']['disabled'])) {
@@ -1803,10 +1802,11 @@ function ec_resp_table($variables) {
         $header_count++;
       }
     }
-    $rows[] = array(array(
-      'data' => $empty,
-      'colspan' => $header_count,
-      'class' => array('empty', 'message'),
+    $rows[] = array(
+      array(
+        'data' => $empty,
+        'colspan' => $header_count,
+        'class' => array('empty', 'message'),
       ),
     );
   }
