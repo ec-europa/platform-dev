f7716bc0e102dc770ffc91968b283c42e5e59a73
Boris Doesborg <bdoesborg@gmail.com>
2016-01-12 10:10:24 +0100

    MULTISITE-8214 coding standards ne_map module

--- profiles/common/modules/features/ne_map/js/ne_map.js
+++ profiles/common/modules/features/ne_map/js/ne_map.js
@@ -29,15 +29,9 @@ L.custom = {
     // @todo fix attribution mess.
     if (settings.attribution.attributionControl == 1) {
       options.attribution = settings.attribution.attribution;
-      //var gj = L.geoJson();
-      //gj.getAttribution = function () {
-      //  return settings.attribution.attribution;
-      //};
-      //gj.addTo(map);
     }
 
-    var tiles = L.wt.tileLayer(settings.tile_layer.tile_layer, options).addTo(map);
-
+    var tiles = L.wt.tileLayer(settings.tiles.tiles, options).addTo(map);
 
     // Defines custom Icon.
     var defaultIcon = L.Icon.extend({
@@ -52,124 +46,19 @@ L.custom = {
       }
     });
 
-    // Checks for node layers, adds node features to map and layer control.
-    // @todo merge with csv layers code??
-    if (typeof Drupal.settings.node_layers !== 'undefined') {
-
-      var node_layers = Drupal.settings.node_layers;
-
-
-      var arrayLength = node_layers.length;
-      for (var i = 0; i < arrayLength; i++) {
-        var one_layer = node_layers[i];
-        // Applies marker clustering.
-        if (node_layers[i].cluster == 1) {
-          var colour = node_layers[i].colour;
-          var pruneCluster = new PruneClusterForLeaflet();
-          pruneCluster.BuildLeafletClusterIcon = function (cluster) {
-            var c = 'prunecluster prunecluster-';
-            var iconSize = 38;
-            var maxPopulation = this.Cluster.GetPopulation();
-            if (cluster.population < Math.max(10, maxPopulation * 0.01)) {
-              c += 'small';
-            }
-            else if (cluster.population < Math.max(100, maxPopulation * 0.05)) {
-              c += 'medium';
-              iconSize = 40;
-            }
-            else {
-              c += 'large';
-              iconSize = 44;
-            }
-            c += ' prunecluster-' + node_layers[i].colour;
-            return new L.DivIcon({
-              html: "<div><span>" + cluster.population + "</span></div>",
-              className: c,
-              iconSize: L.point(iconSize, iconSize)
-            })
-          }
-          var index;
-          var features = node_layers[i].features;
-          if (settings.fitbounds == 1) {
-            var geojson = L.geoJson(features);
-            var padding = parseInt(settings.padding);
-            map.fitBounds(geojson.getBounds(), {padding: [padding, padding]});
-          }
-          for (index = 0; index < features.length; ++index) {
-            var lon = features[index].geometry.coordinates[0];
-            var lat = features[index].geometry.coordinates[1];
-            var marker = new PruneCluster.Marker(lat, lon);
-            marker.data.name = features[index].properties.name;
-            marker.data.icon = L.icon({
-              iconUrl: '//europa.eu/webtools/services/map/images/marker-icon-' + node_layers[i].colour + '.png',
-              shadowUrl: '//europa.eu/webtools/services/map/images//marker-shadow.png'
-            });
-            marker.data.popup = features[index].properties.popupContent;
-            pruneCluster.RegisterMarker(marker);
-          }
-          map.addLayer(pruneCluster);
-          layers.push({"label": node_layers[i].label, "layer": pruneCluster});
-        }
-        else {
-
-          var id = node_layers[i].id;
-          var id = L.geoJson(node_layers[i].features, {
-            onEachFeature: onEachFeature,
-            pointToLayer: function (feature, latlng) {
-
-              // Sets custom marker icon if defined in the feature.
-              if (typeof node_layers[i].colour !== 'undefined') {
-                var icon = new defaultIcon({iconUrl: '//europa.eu/webtools/services/map/images/marker-icon-' + node_layers[i].colour + '.png'});
-              }
-              else {
-                var icon = defaultIcon;
-              }
-              return L.marker(latlng, {icon: icon});
-            }
-          });
-
-          // Puts the layers that are marked "enabled" on the map. The others can
-          // be switched on or off using the layercontrol.
-          if (node_layers[i].enabled == '1') {
-            id.addTo(map);
-          }
-
-          // Adds all layers to the layercontrol.
-          layers.push({"label": node_layers[i].label, "layer": id});
-        }
-      }
-    }
-
     // Enables layers, layer and panel control when they are defined.
     // Cycles through list of layers waiting to be enabled.
     if (typeof layers_to_enable != 'undefined') {
       var arrayLength = layers_to_enable.length;
       for (var i = 0; i < arrayLength; i++) {
-        layers_to_enable[i].layer.addTo(map);
+        // @todo check possible issue addlayer vs addto
+        map.addLayer(layers_to_enable[i].layer);
       }
     }
 
     // Adds layers panel to sidebar when there are layers.
     if (typeof layers_panel != 'undefined') {
       layers_panel.addTo(map);
-    }
-
-    // Adds layers panel to sidebar when there are layers.
-    // @todo figure out if needed
-    //if (typeof layers_control != 'undefined') {
-    //  //layers_control.addTo(map);
-    //}
-
-    // Creates pop up event and defines popup content for each feature.
-    function onEachFeature(feature, layer) {
-      if (feature.properties && feature.properties.popupContent) {
-        if (settings.popup.popin) {
-          layer.bindInfo(feature.properties.popupContent)
-        }
-        else {
-          layer.bindPopup(feature.properties.popupContent);
-        }
-      }
     }
 
     // Processes the next component.
