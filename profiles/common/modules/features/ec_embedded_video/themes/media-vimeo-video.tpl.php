c23fd3c692cddc8f1eb379f6acfe75b6945998f7
Pieter Frenssen <pieter@frenssen.be>
2016-03-03 11:45:31 +0100

    NEXTEUROPA-9481: Fix PHP CodeSniffer warnings.

--- profiles/common/modules/features/ec_embedded_video/themes/media-vimeo-video.tpl.php
+++ profiles/common/modules/features/ec_embedded_video/themes/media-vimeo-video.tpl.php
@@ -17,7 +17,6 @@
  *  $alternative_content - Text to display for browsers that don't support
  *  iframes.
  */
-
 ?>
 <div class="<?php print $classes; ?> media-vimeo-<?php print $id; ?>">
   <iframe class="media-vimeo-player" <?php print $api_id_attribute; ?>width="<?php print $width; ?>" height="<?php print $height; ?>" title="<?php print $title; ?>" src="<?php print $ec_embedded_video_url; ?>" frameborder="0" allowfullscreen><?php print $alternative_content; ?></iframe>
