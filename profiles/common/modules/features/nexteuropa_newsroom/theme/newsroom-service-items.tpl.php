7329ac6c5d74d98ab23d61aa09df79389619c5dd
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-03-17 11:59:44 +0100

    Imporve URL build functions. Remove unnecessary call. Replace calls them with static versions. Remove the obsolete blocks code. Move include files to info. Remove newsroom_side_wide context

--- profiles/common/modules/features/nexteuropa_newsroom/theme/newsroom-service-items.tpl.php
+++ profiles/common/modules/features/nexteuropa_newsroom/theme/newsroom-service-items.tpl.php
@@ -22,7 +22,7 @@
                     <?php echo drupal_render($item->field_field_newsroom_service_image[0]['rendered']); ?>
                 <?php endif; ?>
                 <?php if ($service_id): ?>
-                    <?php $universe_id = variable_get('newsroom_universe_id', FALSE); ?>
+                    <?php $universe_id = NewsroomHelper::getUniverseId(); ?>
                     <?php if ($universe_id): ?>
                       <div class="sample"><?php echo l(t('sample'), 'http://ec.europa.eu/information_society/newsroom/cf/' . $universe_id . '/newsletter-specific-archive.cfm?serviceId=' . $service_id, array('external' => TRUE)); ?></div>
                     <?php endif; ?>
