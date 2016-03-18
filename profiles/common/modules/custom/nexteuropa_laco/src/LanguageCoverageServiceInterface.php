32df90f8a7c5eddb2d61a96e9e061e174b72b72c
Antonio De Marco <antonio@nuvole.org>
2016-03-18 15:05:35 +0100

    NEXTEUROPA-9797: Starts handling HEAD requests.

--- profiles/common/modules/custom/nexteuropa_laco/src/LanguageCoverageServiceInterface.php
+++ profiles/common/modules/custom/nexteuropa_laco/src/LanguageCoverageServiceInterface.php
@@ -14,4 +14,19 @@ namespace Drupal\nexteuropa_laco;
  */
 interface LanguageCoverageServiceInterface {
 
+  /**
+   * Check if current HTTP request should be handled service.
+   *
+   * @return bool
+   *    TRUE if current HTTP request can be handled, FALSE otherwise.
+   */
+  static public function isServiceRequest();
+
+  /**
+   * Deliver response based on current path's language coverage.
+   *
+   * This method sets proper HTTP status codes and exits.
+   */
+  public function deliverResponse();
+
 }
--- profiles/common/modules/custom/nexteuropa_laco/tests/LanguageCoverageServiceTest.php
+++ profiles/common/modules/custom/nexteuropa_laco/tests/LanguageCoverageServiceTest.php
@@ -7,7 +7,8 @@
 
 namespace Drupal\nexteuropa_laco\Tests;
 
-use Drupal\nexteuropa_laco\LanguageCoverageService;
+use Drupal\nexteuropa_laco\LanguageCoverageService as Service;
+
 
 /**
  * Class LanguageCoverageServiceTest.
@@ -17,10 +18,47 @@ use Drupal\nexteuropa_laco\LanguageCoverageService;
 class LanguageCoverageServiceTest extends \PHPUnit_Framework_TestCase {
 
   /**
-   * Smoke test.
+   * Test factory method.
    */
-  public function testSmoke() {
-    $this->assertEquals(LanguageCoverageService::HTTP_METHOD, 'HEAD');
+  public function testFactoryMethod() {
+    $instance = Service::getInstance();
+    $reflection = new \ReflectionClass($instance);
+    $this->assertEquals('Drupal\nexteuropa_laco\LanguageCoverageService', $reflection->getName());
+  }
+
+  /**
+   * Test HTTP request.
+   */
+  public function testHttpRequest() {
+    $client = $this->getClient();
+    $response = $client->request(Service::HTTP_METHOD, $this->getUri('user'));
+    $this->assertEquals(200, $response->getStatusCode());
+    return;
+  }
+
+  /**
+   * Returns Drupal full path.
+   *
+   * @param string $path
+   *    Relative Drupal path.
+   *
+   * @return string
+   *    Full URI.
+   */
+  protected function getUri($path) {
+    return BASE_URL . '/' . $path;
+  }
+
+  /**
+   * Returns Guzzle client object.
+   *
+   * @return \GuzzleHttp\Client
+   *    Properly configured client.
+   */
+  protected function getClient() {
+    return new \GuzzleHttp\Client([
+      'headers' => [Service::HTTP_HEADER_SERVICE_NAME => Service::HTTP_HEADER_SERVICE_VALUE],
+    ]);
   }
 
 }
