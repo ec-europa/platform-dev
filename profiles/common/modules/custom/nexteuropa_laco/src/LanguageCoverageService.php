bca3d7e6677f00981faf6cb1f0f63620cbe3e3a1
Antonio De Marco <antonio@nuvole.org>
2016-03-18 15:05:35 +0100

    NEXTEUROPA-9797: Starts handling HEAD requests.

--- profiles/common/modules/custom/nexteuropa_laco/src/LanguageCoverageService.php
+++ profiles/common/modules/custom/nexteuropa_laco/src/LanguageCoverageService.php
@@ -34,4 +34,33 @@ class LanguageCoverageService implements LanguageCoverageServiceInterface {
    */
   const HTTP_HEADER_LANGUAGE_NAME = 'EC-LACO-lang';
 
+  /**
+   * {@inheritdoc}
+   */
+  public function deliverResponse() {
+    drupal_add_http_header('Status', '200 OK');
+    drupal_add_http_header('Content-Length', '0');
+    drupal_exit();
+  }
+
+  /**
+   * Factory method.
+   *
+   * @return \Drupal\nexteuropa_laco\LanguageCoverageServiceInterface
+   */
+  static public function getInstance() {
+    return new static();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static public function isServiceRequest() {
+    $service_header = 'HTTP_' . strtoupper(self::HTTP_HEADER_SERVICE_NAME);
+    $service_header = str_replace('-', '_', $service_header);
+    $result = isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] == self::HTTP_METHOD);
+    $result = $result && isset($_SERVER[$service_header]) && ($_SERVER[$service_header] == self::HTTP_HEADER_SERVICE_VALUE);
+    return $result;
+  }
+
 }
