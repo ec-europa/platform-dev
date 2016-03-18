305a8368e8401ea2b8fe035ec058c8137616556a
Antonio De Marco <antonio@nuvole.org>
2016-03-18 13:57:52 +0100

    NEXTEUROPA-9797: Initial work on nexteuropa_laco module.

---
+++ profiles/common/modules/custom/nexteuropa_laco/src/LanguageCoverageServiceInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_laco\LanguageCoverageServiceInterface.
+ */
+
+namespace Drupal\nexteuropa_laco;
+
+/**
+ * Interface LanguageCoverageServiceInterface.
+ *
+ * @package Drupal\nexteuropa_laco
+ */
+interface LanguageCoverageServiceInterface {
+
+}
---
+++ profiles/common/modules/custom/nexteuropa_laco/tests/LanguageCoverageServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_laco\Tests\LanguageCoverageServiceTest.
+ */
+
+namespace Drupal\nexteuropa_laco\Tests;
+
+/**
+ * Class LanguageCoverageServiceTest.
+ *
+ * @package Drupal\nexteuropa_laco\Tests
+ */
+class LanguageCoverageServiceTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmoke() {
+
+  }
+
+}
