55fcda20aab543c76e1cdae8628b05227855137c
Yannick Huard <huard.yan@gmail.com>
2016-04-15 16:20:58 +0200

    Fix coding standards

--- tests/src/Context/PhantomjsContext.php
+++ tests/src/Context/PhantomjsContext.php
@@ -11,30 +11,31 @@ use Behat\Behat\Context\Context;
 use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 use PhantomInstaller\PhantomBinary;
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
-/*
- * Start PhantomJS before the tests run
+/**
+ * Start PhantomJS before the tests run.
  *
- * Don't try to stop the process or you will get an exception
+ * Don't try to stop the process or you will get an exception:
  *   [Behat\Mink\Exception\DriverException]
  *   Could not close connection
- * Just let it go, Symfony\Component\Process\Process will stop it anyway
+ * Just let it go, Symfony\Component\Process\Process will stop it anyway.
  */
-class PhantomjsContext implements Context
-{
-     private static $process;
+class PhantomjsContext implements Context {
 
-     /**
-      * @BeforeSuite
-      */
-     public static function startPhantomjs(BeforeSuiteScope $scope)
-     {
-          $cmd = sprintf('exec %s --webdriver=%d', PhantomBinary::BIN, 8643);
-          self::$process = new Process($cmd);
-          self::$process->start();
+  private static $process;
 
-          // Wait 1 sec to let PhantomJS start
-          sleep(1);
-     }
+  /**
+   * Start PhantomJS.
+   *
+   * @BeforeSuite
+   */
+  public static function startPhantomjs(BeforeSuiteScope $scope) {
+    $cmd = sprintf('exec %s --webdriver=%d', PhantomBinary::BIN, 8643);
+    self::$process = new Process($cmd);
+    self::$process->start();
+
+    // Wait 1 sec to let PhantomJS start.
+    sleep(1);
+  }
+
 }
