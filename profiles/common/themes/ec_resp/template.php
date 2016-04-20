2fb9e3f71279c283a9b232d0c29262e02de1e39f
GERVAIS Sebastien <sebastien.gervais@ext.ec.europa.eu>
2016-04-20 16:31:04 +0200

    Fix coding standard

--- profiles/common/themes/ec_resp/template.php
+++ profiles/common/themes/ec_resp/template.php
@@ -353,8 +353,11 @@ function ec_resp_preprocess_node(&$variables) {
 
 }
 
-function ec_resp_preprocess_file_entity(&$variables){
-  if ($variables['view_mode'] == "media_gallery_colorbox"){
+/**
+ * Implements template_preprocess_file_entity().
+ */
+function ec_resp_preprocess_file_entity(&$variables) {
+  if ($variables['view_mode'] == "media_gallery_colorbox") {
     $variables['classes_array'][] = "col-xs-3";
   }
 }
