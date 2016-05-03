b505a55025ed8d40ab75d7d6176451b0d8068ee9
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-05-03 15:20:00 +0200

    Nexteuropa Newsroom files

---
+++ profiles/common/modules/features/nexteuropa_newsroom/theme/newsroom-summary-block.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Block summary template.
+ */
+?>
+<div class="block-nexteuropa-newsroom">
+  <div class="newsroom_rss">
+    <?php echo $rss; ?>
+  </div>
+  <?php echo $content; ?>
+</div>
