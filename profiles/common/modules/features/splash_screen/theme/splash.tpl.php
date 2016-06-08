5f25ac05cad8bb303a30f7d5271bfc6da3b43d69
Yannick Huard <huard.yan@gmail.com>
2016-06-08 13:57:42 +0200

    Make splash screen and language switcher (site) look the same

--- profiles/common/modules/features/splash_screen/theme/splash.tpl.php
+++ profiles/common/modules/features/splash_screen/theme/splash.tpl.php
@@ -16,14 +16,10 @@
  */
 ?>
 
-<div class="row">
-  <div class="col-md-8 col-md-offset-2">
-    <nav class="site-level-language-selector">
-      <?php print $icon; ?>
-      <?php print $languages_list; ?>
-      <?php if (isset($close_button)): ?>
-        <?php print $close_button; ?>
-      <?php endif; ?>
-    </nav>
-  </div>
-</div>
+<nav class="site-level-language-selector">
+  <?php print $icon; ?>
+  <?php print $languages_list; ?>
+  <?php if (isset($close_button)): ?>
+    <?php print $close_button; ?>
+  <?php endif; ?>
+</nav>
