2402ee517d9744060f2c6060ebb2a78d900d4409
Kristof Coomans <kristof@2dotstwice.be>
2016-07-20 08:48:08 +0200

    NEXTEUROPA-10404: Add back namespace declaration to Poetry html document

--- profiles/common/modules/custom/tmgmt_dgt_html/templates/tmgmt_dgt_html_template.tpl.php
+++ profiles/common/modules/custom/tmgmt_dgt_html/templates/tmgmt_dgt_html_template.tpl.php
@@ -4,7 +4,7 @@
  * Template of the DGT supported HTML format.
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="JobID" content="<?php echo filter_xss($tjid); ?>" />
