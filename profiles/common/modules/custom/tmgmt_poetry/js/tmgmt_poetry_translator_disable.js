7cbc09772ef70863da73bc11d3dff80a17e9c36e
netlooker <czarny@data.pl>
2016-08-16 15:11:29 +0200

    NEXTEUROPA-7719: cleaning JS script to include codacy suggestions

--- profiles/common/modules/custom/tmgmt_poetry/js/tmgmt_poetry_translator_disable.js
+++ profiles/common/modules/custom/tmgmt_poetry/js/tmgmt_poetry_translator_disable.js
@@ -6,18 +6,18 @@
 (function($) {
     Drupal.behaviors.tmgmtPoetryDisableTranslator = {
         attach: function (context, settings) {
-            $('#edit-translator').attr("disabled", 'disabled');
-            $('#edit-translator').parent().addClass('form-disabled');
+            $("#edit-translator").attr("disabled", "disabled");
+            $("#edit-translator").parent().addClass("form-disabled");
 
-          if (!$('#unblock-translator').length) {
-              $('#edit-translator').parent().append("<a href='#' id='unblock-translator'>Change translator</a>");
-          }
-
-            $('#unblock-translator').click(function(e) {
-                    $('#edit-translator').removeAttr("disabled", 'disabled');
-                    $('#edit-translator').parent().removeClass('form-disabled');
-                    $(this).remove();
+            if (!$("#unblock-translator").length) {
+                $("#edit-translator").parent().append("<a href='#' id='unblock-translator'>Change translator</a>");
             }
+
+            $("#unblock-translator").click(function(e) {
+                    $("#edit-translator").removeAttr("disabled", "disabled");
+                    $("#edit-translator").parent().removeClass("form-disabled");
+                    $(this).remove();
+                }
             );
         }
   };
