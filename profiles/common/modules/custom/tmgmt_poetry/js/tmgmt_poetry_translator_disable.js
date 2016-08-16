035432fb6066c26fd231641b15c98c2ca1bd004a
netlooker <czarny@data.pl>
2016-08-16 15:13:37 +0200

    NEXTEUROPA-7719 adding formating coming from PHP Code sniffer

--- profiles/common/modules/custom/tmgmt_poetry/js/tmgmt_poetry_translator_disable.js
+++ profiles/common/modules/custom/tmgmt_poetry/js/tmgmt_poetry_translator_disable.js
@@ -9,15 +9,15 @@
             $("#edit-translator").attr("disabled", "disabled");
             $("#edit-translator").parent().addClass("form-disabled");
 
-            if (!$("#unblock-translator").length) {
-                $("#edit-translator").parent().append("<a href='#' id='unblock-translator'>Change translator</a>");
-            }
+          if (!$("#unblock-translator").length) {
+              $("#edit-translator").parent().append("<a href='#' id='unblock-translator'>Change translator</a>");
+          }
 
             $("#unblock-translator").click(function(e) {
                     $("#edit-translator").removeAttr("disabled", "disabled");
                     $("#edit-translator").parent().removeClass("form-disabled");
                     $(this).remove();
-                }
+            }
             );
         }
   };
