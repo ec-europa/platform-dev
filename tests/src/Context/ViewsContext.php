68f7f072a8b8f63dfa6eff10cb39fcac1624b1de
Antonio De Marco <antonio@nuvole.org>
2016-10-13 16:52:27 +0200

    feat(multilingualism): NEPT-107 - Adding views Behat context for importing views via Behat steps.

---
+++ tests/src/Context/ViewsContext.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa\Context\ViewsContext
+ */
+
+namespace Drupal\nexteuropa\Context;
+
+use Behat\Gherkin\Node\PyStringNode;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use function \bovigo\assert\assert;
+use function \bovigo\assert\predicate\isNotNull;
+
+/**
+ * Class ViewsContext.
+ *
+ * @package Drupal\nexteuropa\Context
+ */
+class ViewsContext extends RawDrupalContext {
+
+  /**
+   * Views created during test execution.
+   *
+   * @var \view[]
+   */
+  protected $views = [];
+
+  /**
+   * @Given I import the following view:
+   */
+  public function importView(PyStringNode $view_export) {
+    /** @var \view $view */
+    $view = NULL;
+
+    // This is actually how Views imports exported views.
+    // @see views_ui_import_validate()
+    ob_start();
+    eval($view_export->getRaw());
+    ob_end_clean();
+
+    assert($view, isNotNull());
+    $view->vid = 'new';
+    $view->save();
+    $this->views[] = $view;
+  }
+
+  /**
+   * @AfterScenario
+   */
+  public function removeViews() {
+    foreach ($this->views as $view) {
+      views_delete_view($view);
+    }
+  }
+
+}
