bb65238f6a0772e5d08eb2bc4e05433c3ebb59f8
Pol Dellaiera <pol.dellaiera@gmail.com>
2016-10-18 15:19:02 +0200

    Adding Utility classes for generating machine names and random strings.

--- tests/src/Context/BeanContext.php
+++ tests/src/Context/BeanContext.php
@@ -8,6 +8,7 @@
 namespace Drupal\nexteuropa\Context;
 
 use Behat\Behat\Context\Context;
+use Drupal\nexteuropa\Component\Utility\Transliterate;
 
 /**
  * Context for configuring the Bean.
@@ -15,17 +16,25 @@ use Behat\Behat\Context\Context;
 class BeanContext implements Context {
 
   /**
-   * A random string.
+   * The transliterate utility object.
    *
-   * @var string
+   * @var \Drupal\nexteuropa\Component\Utility\Transliterate
    */
-  protected $randomString;
+  protected $transliterate;
+
+  /**
+   * The random utility object.
+   *
+   * @var \Rych\Random\Random
+   */
+  protected $random;
 
   /**
    * BeanContext constructor.
    */
   public function __construct() {
-    $this->randomString = uniqid();
+    $this->transliterate = new Transliterate();
+    $this->random = new \Rych\Random\Random();
   }
 
   /**
@@ -41,75 +50,10 @@ class BeanContext implements Context {
     $plugin_info['name'] = '';
 
     $bean_type = new \BeanCustom($plugin_info);
-    $bean_type->type = $this->getRandomMachineName($type);
+    $bean_type->type = substr(str_pad(substr($this->transliterate->getMachineName($type), 0, 32), 32, '_'), 0, 23) . '_' . $this->transliterate->getMachineName($this->random->getRandomString(8));
     $bean_type->setLabel($type);
     $bean_type->setDescription('Behat');
     $bean_type->save(TRUE);
-  }
-
-  /**
-   * Set all permissions to admin role.
-   *
-   * @Given I update the administrator role permissions
-   */
-  public function iGiveAllPermToAdminRole() {
-    if ($rid = variable_get('user_admin_role', 0)) {
-      $perms = array();
-
-      foreach (module_implements('permission') as $module) {
-        foreach(module_invoke($module, 'permission') as $key => $perm) {
-          $perms[$key] = $module;
-        }
-      }
-
-      if ($perms) {
-        foreach ($perms as $perm => $module) {
-          $query = db_merge('role_permission');
-          $query->key(array(
-            'rid' => $rid,
-            'permission' => $perm,
-          ));
-          $query->fields(array(
-            'rid' => $rid,
-            'permission' => $perm,
-            'module' => $module,
-          ));
-          $query->execute();
-        }
-      }
-    }
-  }
-
-  /**
-   * Return a random type name.
-   *
-   * @param string $type
-   *   The block type.
-   *
-   * @return string
-   *   The random block type.
-   */
-  public function getRandomMachineName($type) {
-    $type = str_pad($this->getMachineName($type), 32, '_');
-    $type = substr($type, 0, 23) . '_' . substr($this->randomString, 0, 8);
-
-    return $type;
-  }
-
-  /**
-   * Create a machine name.
-   *
-   * @param string $name
-   *    Name.
-   *
-   * @return string
-   *    Machine name.
-   */
-  private function getMachineName($name) {
-    $a = explode(",", " ,&,à,á,â,ã,ä,å,æ,ç,è,é,ê,ë,ì,í,î,ï,ñ,ò,ó,ô,õ,ö,ø,ù,ú,û,ü,ý,ÿ,ā,ă,ą,ć,ĉ,ċ,č,ď,đ,ē,ĕ,ė,ę,ě,ĝ,ğ,ġ,ģ,ĥ,ħ,ĩ,ī,ĭ,į,ı,ĳ,ĵ,ķ,ĺ,ļ,ľ,ŀ,ł,ń,ņ,ň,ŉ,ō,ŏ,ő,œ,ŕ,ŗ,ř,ś,ŝ,ş,š,ţ,ť,ŧ,ũ,ū,ŭ,ů,ű,ų,ŵ,ŷ,ź,ż,ž,ƒ,ơ,ư,ǎ,ǐ,ǒ,ǔ,ǖ,ǘ,ǚ,ǜ,ǻ,ǽ,ǿ,ά,έ,ό,Ώ,ώ,ί,ϊ,ΐ,ύ,ϋ,ΰ,ή");
-    $b = explode(",", "_,_,a,a,a,a,a,a,ae,c,e,e,e,e,i,i,i,i,n,o,o,o,o,o,o,u,u,u,u,y,y,a,a,a,c,c,c,c,d,d,e,e,e,e,e,g,g,g,g,h,h,i,i,i,i,i,ij,j,k,l,l,l,l,l,l,n,n,n,n,o,o,o,oe,r,r,r,s,s,s,s,t,t,t,u,u,u,u,u,u,w,y,z,z,z,s,f,o,u,a,i,o,u,u,u,u,u,a,ae,o,α,ε,ο,Ω,ω,ι,ι,ι,υ,υ,υ,η");
-    $machine_name = str_replace($a, $b, strtolower($name));
-    return $machine_name;
   }
 
 }
--- tests/src/Context/MinkContext.php
+++ tests/src/Context/MinkContext.php
@@ -9,6 +9,7 @@ namespace Drupal\nexteuropa\Context;
 
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Element\Element;
 use Behat\Mink\Exception\ElementNotFoundException;
 use Behat\Mink\Exception\ExpectationException;
 use Behat\Mink\Selector\Xpath\Escaper;
@@ -148,6 +149,123 @@ class MinkContext extends DrupalExtensionMinkContext {
    */
   public function maximizeBrowserWindow() {
     $this->getSession()->getDriver()->maximizeWindow();
+  }
+
+  /**
+   * Attempts to find and check a checkbox in a table row containing given text.
+   *
+   * @param string $row_text
+   *    Text on the table row.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *    Throw exception if class table row was not found.
+   *
+   * @Given I check the box on the :row_text row
+   */
+  public function checkCheckboxOnTableRow($row_text) {
+    $page = $this->getSession()->getPage();
+    if ($checkbox = $this->getTableRow($page, $row_text)->find('css', 'input[type=checkbox]')) {
+      $checkbox->check();
+      return;
+    }
+    throw new ExpectationException(sprintf('Found a row containing "%s", but no "%s" link on the page %s', $row_text, $checkbox, $this->getSession()->getCurrentUrl()), $this->getSession());
+  }
+
+  /**
+   * Checks a checkbox in a table row inside a specific fieldset.
+   *
+   * @param string $fieldset_locator
+   *   Fieldset id or legend.
+   * @param string $row_text
+   *   Text on the table row.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   *   When the fieldset or field are not found.
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *    Throw exception if class table row was not found.
+   *
+   * @When /^inside fieldset "(?P<fieldset_locator>(?:[^"]|\\")*)" I check the box on the "(?P<row_text>(?:[^"]|\\")*)" row$/
+   */
+  public function checkCheckboxInsideFieldsetOnTableRow($fieldset_locator, $row_text) {
+    $fieldset = $this->findFieldset($fieldset_locator);
+
+    if (!$fieldset) {
+      throw new ElementNotFoundException(
+        $this->getSession()->getDriver(),
+        'fieldset', 'id|legend',
+        $fieldset_locator
+      );
+    }
+
+    if ($checkbox = $this->getTableRow($fieldset, $row_text)
+      ->find('css', 'input[type=checkbox]')
+    ) {
+      $checkbox->check();
+      return;
+    }
+    throw new ExpectationException(sprintf('Found a row containing "%s", but no "%s" link on the page %s', $row_text, $checkbox, $this->getSession()
+      ->getCurrentUrl()), $this->getSession());
+  }
+
+  /**
+   * Retrieve a table row containing specified text from a given element.
+   *
+   * @param Element $element
+   *    Mink element object.
+   * @param string $search
+   *    Table row text.
+   *
+   * @throws \Exception
+   *    Throw exception if class table row was not found.
+   *
+   * @return NodeElement
+   *    Table row node element.
+   */
+  public function getTableRow(Element $element, $search) {
+    $rows = $element->findAll('css', 'tr');
+    if (empty($rows)) {
+      throw new \Exception(sprintf('No rows found on the page %s', $this->getSession()->getCurrentUrl()));
+    }
+    /** @var NodeElement $row */
+    foreach ($rows as $row) {
+      if (strpos($row->getText(), $search) !== FALSE) {
+        return $row;
+      }
+    }
+    throw new \Exception(sprintf('Failed to find a row containing "%s" on the page %s', $search, $this->getSession()->getCurrentUrl()));
+  }
+
+  /**
+   * Set all permissions to admin role.
+   *
+   * @Given I update the administrator role permissions
+   */
+  public function iGiveAllPermToAdminRole() {
+    if ($rid = variable_get('user_admin_role', 0)) {
+      $perms = array();
+
+      foreach (module_implements('permission') as $module) {
+        foreach (module_invoke($module, 'permission') as $key => $perm) {
+          $perms[$key] = $module;
+        }
+      }
+
+      if ($perms) {
+        foreach ($perms as $perm => $module) {
+          $query = db_merge('role_permission');
+          $query->key(array(
+            'rid' => $rid,
+            'permission' => $perm,
+          ));
+          $query->fields(array(
+            'rid' => $rid,
+            'permission' => $perm,
+            'module' => $module,
+          ));
+          $query->execute();
+        }
+      }
+    }
   }
 
 }
