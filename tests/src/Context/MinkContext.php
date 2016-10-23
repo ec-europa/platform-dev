567937e1a4c08ac2004297a1e1917a241598a764
Frederic Dewinne <frederic@continuousphp.com>
2016-10-23 21:55:33 +0200

    fix file attached issue

--- tests/src/Context/MinkContext.php
+++ tests/src/Context/MinkContext.php
@@ -150,4 +150,32 @@ class MinkContext extends DrupalExtensionMinkContext {
     $this->getSession()->getDriver()->maximizeWindow();
   }
 
+    public function attachFileToField($field, $path) {
+        $field = $this->fixStepArgument($field);
+
+        if ($this->getMinkParameter('files_path')) {
+            $fullPath = rtrim(realpath($this->getMinkParameter('files_path')), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$path;
+            if (is_file($fullPath)) {
+                $path = $fullPath;
+            }
+        }
+
+        if (!$this->getSession()->getDriver() instanceof Behat\Mink\Driver\GoutteDriver) {
+            $tempZip = tempnam('', 'WebDriverZip');
+            $zip = new \ZipArchive();
+            $zip->open($tempZip, \ZipArchive::CREATE);
+            $zip->addFile($path, basename($path));
+            $zip->close();
+            $path = $this->getSession()->getDriver()->getWebDriverSession()->file([
+                'file' => base64_encode(file_get_contents($tempZip))
+            ]);
+        }
+
+        $this->getSession()->getPage()->attachFileToField($field, $path);
+
+        if (isset($tempZip)) {
+            unlink($tempZip);
+        }
+    }
+
 }
