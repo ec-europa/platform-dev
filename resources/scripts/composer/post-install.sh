3965886aa7fe443dd6766ed960634911a5a0994c
Kristof Coomans <kristof@2dotstwice.be>
2016-10-25 08:04:00 +0200

    chore(configuration): remove incompatible d option for cp

--- resources/scripts/composer/post-install.sh
+++ resources/scripts/composer/post-install.sh
@@ -9,4 +9,4 @@ ln -s "../../vendor/pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
 # Copy PHPCompatibility coding standard over to CodeSniffer directory.
 # See https://github.com/wimg/PHPCompatibility/issues/102.
 rm -rf vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
-cp -rd vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
+cp -r vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
--- resources/scripts/composer/post-update.sh
+++ resources/scripts/composer/post-update.sh
@@ -3,4 +3,4 @@
 # Copy PHPCompatibility coding standard over to CodeSniffer directory.
 # See https://github.com/wimg/PHPCompatibility/issues/102.
 rm -rf vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
-cp -rd vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
+cp -r vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
