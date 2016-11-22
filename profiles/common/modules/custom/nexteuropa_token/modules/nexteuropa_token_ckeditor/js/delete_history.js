bafd99f8726a007b2b27e3894290919c7510686c
Pol Dellaiera <pol.dellaiera@gmail.com>
2016-11-22 15:52:00 +0100

    Fix PHPCS.

--- profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/js/delete_history.js
+++ profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/js/delete_history.js
@@ -2,6 +2,8 @@
  * @file
  * NEPT-91: Delete the History object in order to avoid errors when loading some views with Ajax.
  */
+
 History = {
-    pushState: function() {}
+    pushState: function() {
+    }
 };
