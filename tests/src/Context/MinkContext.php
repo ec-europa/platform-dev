9811a1f435632c7a21ae2e1ae4f7c2587a456ed0
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2016-12-14 11:09:34 +0100

    Revert "feat(communities) Remove duplicated step caused by the merge from communities to 2.3"
    This reverts commit 9dc010af4e451128858ff59b527d9a99eba3ce7d.

--- tests/src/Context/MinkContext.php
+++ tests/src/Context/MinkContext.php
@@ -269,7 +269,27 @@ class MinkContext extends DrupalExtensionMinkContext {
 
     return NULL;
   }
-  
+
+  /**
+   * Attempts to find and check a checkbox in a table row containing given text.
+   *
+   * @param string $row_text
+   *    Text on the table row.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *    Throw exception if class table row was not found.
+   *
+   * @Given I check the box on the :row_text row
+   */
+  public function checkCheckboxOnTableRow($row_text) {
+    $page = $this->getSession()->getPage();
+    if ($checkbox = $this->getTableRow($page, $row_text)->find('css', 'input[type=checkbox]')) {
+      $checkbox->check();
+      return;
+    }
+    throw new ExpectationException(sprintf('Found a row containing "%s", but no "%s" link on the page %s', $row_text, $checkbox, $this->getSession()->getCurrentUrl()), $this->getSession());
+  }
+
   /**
    * Checks a checkbox in a table row inside a specific fieldset.
    *
