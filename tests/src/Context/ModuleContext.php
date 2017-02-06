f1b26aaad807d001a92b02d0ee011d8a07cdb3da
gillesdeudon <gdeudon@hotmail.com>
2017-02-06 19:00:51 +0100

    fix(Configuration & maintenance): NEPT-509 - Change the order of context declaration and AfterScenario methods

--- tests/src/Context/ModuleContext.php
+++ tests/src/Context/ModuleContext.php
@@ -29,65 +29,6 @@ class ModuleContext extends RawDrupalContext {
   protected $testActivatedFeatureSets = array();
 
   /**
-   * Enables one or more Feature Set(s).
-   *
-   * Provide feature set names in the following format:
-   *
-   * | featureSet  |
-   * | Events      |
-   * | Links       |
-   *
-   * @param TableNode $featureset_table
-   *   The table listing feature set titles.
-   *
-   * @throws \Exception
-   *    Throws exception if the feature_set activation failed.
-   *
-   * @Given the/these featureSet/FeatureSets is/are enabled
-   */
-  public function enableFeatureSet(TableNode $featureset_table) {
-    $message = array();
-    $featuresets = feature_set_get_featuresets();
-    foreach ($featureset_table->getHash() as $row) {
-      foreach ($featuresets as $featureset_available) {
-        if ($featureset_available['title'] == $row['featureSet'] &&
-          feature_set_status($featureset_available) === FEATURE_SET_DISABLED) {
-          if (feature_set_enable_feature_set($featureset_available)) {
-            $this->testActivatedFeatureSets[] = $featureset_available;
-          }
-          else {
-            $message[] = $row['featureSet'];
-          }
-        }
-      }
-    }
-
-    if (!empty($message)) {
-      throw new \Exception(sprintf('Feature Set "%s" not correctly enabled', implode(', ', $message)));
-    }
-  }
-
-  /**
-   * Disables one or more Feature Set(s).
-   *
-   * Disable any Feature Set that were enabled during Feature test.
-   *
-   * @AfterScenario
-   */
-  public function cleanFeatureSet() {
-    if (!empty($this->testActivatedFeatureSets)) {
-      // Disable and uninstall any feature set that were enabled.
-      foreach ($this->testActivatedFeatureSets as $featureset) {
-        if (isset($featureset['disable'])) {
-          $featureset['uninstall'] = $featureset['disable'];
-          feature_set_disable_feature_set($featureset);
-        }
-      }
-      $this->testActivatedFeatureSets = array();
-    }
-  }
-
-  /**
    * Enables one or more modules.
    *
    * Provide modules data in the following format:
@@ -215,6 +156,65 @@ class ModuleContext extends RawDrupalContext {
       }
       // Ensure that it is correctly uninstalled.
       drupal_uninstall_modules(array($module_name));
+    }
+  }
+
+  /**
+   * Enables one or more Feature Set(s).
+   *
+   * Provide feature set names in the following format:
+   *
+   * | featureSet  |
+   * | Events      |
+   * | Links       |
+   *
+   * @param TableNode $featureset_table
+   *   The table listing feature set titles.
+   *
+   * @throws \Exception
+   *    Throws exception if the feature_set activation failed.
+   *
+   * @Given the/these featureSet/FeatureSets is/are enabled
+   */
+  public function enableFeatureSet(TableNode $featureset_table) {
+    $message = array();
+    $featuresets = feature_set_get_featuresets();
+    foreach ($featureset_table->getHash() as $row) {
+      foreach ($featuresets as $featureset_available) {
+        if ($featureset_available['title'] == $row['featureSet'] &&
+          feature_set_status($featureset_available) === FEATURE_SET_DISABLED) {
+          if (feature_set_enable_feature_set($featureset_available)) {
+            $this->testActivatedFeatureSets[] = $featureset_available;
+          }
+          else {
+            $message[] = $row['featureSet'];
+          }
+        }
+      }
+    }
+
+    if (!empty($message)) {
+      throw new \Exception(sprintf('Feature Set "%s" not correctly enabled', implode(', ', $message)));
+    }
+  }
+
+  /**
+   * Disables one or more Feature Set(s).
+   *
+   * Disable any Feature Set that were enabled during Feature test.
+   *
+   * @AfterScenario
+   */
+  public function cleanFeatureSet() {
+    if (!empty($this->testActivatedFeatureSets)) {
+      // Disable and uninstall any feature set that were enabled.
+      foreach ($this->testActivatedFeatureSets as $featureset) {
+        if (isset($featureset['disable'])) {
+          $featureset['uninstall'] = $featureset['disable'];
+          feature_set_disable_feature_set($featureset);
+        }
+      }
+      $this->testActivatedFeatureSets = array();
     }
   }
 
