5cd0119258d78495d82b64e286634179b2818c43
Antonio De Marco <antonio@nuvole.org>
2017-03-31 13:31:24 +0200

    NEPT-250: Initial work for PHPUnit integration.

---
+++ profiles/common/modules/features/nexteuropa_core/src/Tests/SmokeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_core\Tests\SmokeTest.
+ */
+
+namespace Drupal\nexteuropa_core\Tests;
+
+use Drupal\nexteuropa\Unit\AbstractUnitTest;
+
+/**
+ * Class SmokeTest.
+ *
+ * @package Drupal\nexteuropa_core\Tests
+ */
+class SmokeTest extends AbstractUnitTest {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmoke() {
+    $this->assertTrue(TRUE);
+  }
+
+}
---
+++ tests/src/Unit/AbstractUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\nexteuropa\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractUnitTest.
+ *
+ * @package Drupal\nexteuropa\Unit
+ */
+abstract class AbstractUnitTest extends TestCase {
+
+}
