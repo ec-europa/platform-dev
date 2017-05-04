3ef12f2a7f503ec7720c507f8991894932e55622
gillesdeudon <gdeudon@hotmail.com>
2017-05-04 09:53:41 +0200

    NEPT-862: Add the XML declartion in tmgmt_dgt_html_template.tpl.php

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_html/templates/tmgmt_dgt_html_template.tpl.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_html/templates/tmgmt_dgt_html_template.tpl.php
@@ -3,10 +3,18 @@
  * @file
  * Template of the DGT supported HTML format.
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?><?php
+// NEPT-862: Add XML declaration above the HTML structure to force the
+// UTF-8 encoding.
+$xml = <<<XML
+<?xml version="1.0" encoding="utf-8" ?>\n
+XML;
+print $xml;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="JobID" content="<?php echo filter_xss($tjid); ?>" />
     <meta name="languageSource" content="<?php echo filter_xss($source_language); ?>" />
     <meta name="languageTarget" content="<?php echo filter_xss($target_language); ?>" />
