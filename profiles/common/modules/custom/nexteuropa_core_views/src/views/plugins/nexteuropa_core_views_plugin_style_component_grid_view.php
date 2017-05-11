9fb3738a9f532889a13d9b2e2360431438987d45
Pol Dellaiera <pol.dellaiera@protonmail.com>
2017-05-11 16:27:33 +0200

    NEPT-970: Add a style plugin for the Grid component.
    Warning, this needs an update in ec-europa-theme repository.

---
+++ profiles/common/modules/custom/nexteuropa_core_views/src/views/plugins/nexteuropa_core_views_plugin_style_component_grid_view.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Definition of a Views style plugin.
+ */
+
+/**
+ * Class to define a row plugin handler.
+ *
+ * @codingStandardsIgnoreFile
+ *
+ * @ingroup views_style_plugins
+ */
+class nexteuropa_core_views_plugin_style_component_grid_view extends views_plugin_style_grid {
+
+}
