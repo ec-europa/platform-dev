1871c4b7e4cb942151f40868045a1f9965d75e5c
amelo <arito.melo@timeinc.com>
2017-10-04 15:40:26 +0200

    Fix QA automation rules - Return type "int | NULL" must not contain spaces.
       ## [NEPT-1389](https://webgate.ec.europa.eu/CITnet/jira/browse/NEPT-1389)
    
       ### Description
       Fix QA automation rules - Return type "int | NULL" must not contain spaces.
    
       ### Change log
    - Removed: The rule
    
        <!-- Return type "int | NULL" must not contain spaces. -->
        <rule ref="Drupal.Commenting.FunctionComment.ReturnTypeSpaces">
            <exclude-pattern>*</exclude-pattern>
        </rule>
    
        - Removed: The unnecessary spaces.
    
        -Created:  An exception has been set to exclude nexteuropa_newsroom module once it is not supported by Core team, so to progress with the clean up for
         QA AUTOMATION (NEPT-884, NEPT-1387 and NEPT-1389), this exception must exist
         so when remove the rule "Drupal.Commenting.FunctionComment.ParamMissingDefinition" the phpcs is OK (all green).
    
        ### Commands
    
      git fetch --all
      git checkout nept-1389
      ./bin/phpcs

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_poetry/tmgmt_poetry_mock/src/Mock/PoetryMock.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_poetry/tmgmt_poetry_mock/src/Mock/PoetryMock.php
@@ -172,7 +172,7 @@ class PoetryMock {
    * @param string $lg_code
    *    Language code. If ALL then all languages will be processed one by one.
    *
-   * @return array Array with translation response data.
+   * @return array
    *    Array with translation response data.
    */
   public static function prepareTranslationResponseData($message, $lg_code) {
@@ -213,7 +213,7 @@ class PoetryMock {
    * @param string $message
    *    Translation request XML data.
    *
-   * @return array Array with translation response data.
+   * @return array
    *    Array with translation response data.
    */
   public static function prepareRefuseJobResponseData($message) {
