8d73c845dc7231a6d3227c25971aac1b387b9370
Pol Dellaiera <pol.dellaiera@protonmail.com>
2017-10-05 12:34:42 +0200

    NEPT-1408: Fix QA automation rules - Expected 1 space after FUNCTION keyword.

--- profiles/common/modules/features/social_bookmark/scripts/social_bookmark.form.js
+++ profiles/common/modules/features/social_bookmark/scripts/social_bookmark.form.js
@@ -4,40 +4,40 @@
  */
 
 (function (jQuery) {
-  jQuery(document).ready(function(){
+  jQuery(document).ready(function (){
     show_hide_field(jQuery('#edit-language-socialbar-language-specified'), jQuery('.form-item-language-selector-socialbar'));
 
-    jQuery('#edit-language-socialbar').live('change',function(){
+    jQuery('#edit-language-socialbar').live('change',function (){
       show_hide_field(jQuery('#edit-language-socialbar-language-specified'), jQuery('.form-item-language-selector-socialbar'));
     });
 
     show_hide_field(jQuery('#edit-language-socialbookmark-language-specified'), jQuery('.form-item-language-selector-socialbookmark'));
 
-    jQuery('#edit-language-socialbookmark').live('change',function(){
+    jQuery('#edit-language-socialbookmark').live('change',function (){
       show_hide_field(jQuery('#edit-language-socialbookmark-language-specified'), jQuery('.form-item-language-selector-socialbookmark'));
     });
 
     show_hide_field(jQuery('#edit-type-socialbookmark-icon'), jQuery('.form-item-icon-size-socialbookmark'));
 
-    jQuery('#edit-type-socialbookmark').live('change',function(){
+    jQuery('#edit-type-socialbookmark').live('change',function (){
       show_hide_field(jQuery('#edit-type-socialbookmark-icon'), jQuery('.form-item-icon-size-socialbookmark'));
     });
 
     show_hide_field(jQuery('#edit-override-socialbookmark'), jQuery('.form-item-label-socialbookmark'));
 
-    jQuery('#edit-override-socialbookmark').live('change',function(){
+    jQuery('#edit-override-socialbookmark').live('change',function (){
       show_hide_field(jQuery('#edit-override-socialbookmark'), jQuery('.form-item-label-socialbookmark'));
     });
 
     show_hide_field(jQuery('#edit-display-socialbookmark'), jQuery('.form-item-least-socialbookmark'));
 
-    jQuery('#edit-display-socialbookmark').live('change',function(){
+    jQuery('#edit-display-socialbookmark').live('change',function (){
       show_hide_field(jQuery('#edit-display-socialbookmark'), jQuery('.form-item-least-socialbookmark'));
     });
 
     show_hide_field(jQuery('#edit-display-socialbookmark'), jQuery('.form-item-orientation-socialbookmark'));
 
-    jQuery('#edit-display-socialbookmark').live('change',function(){
+    jQuery('#edit-display-socialbookmark').live('change',function (){
       show_hide_field(jQuery('#edit-display-socialbookmark'), jQuery('.form-item-orientation-socialbookmark'));
     });
 
