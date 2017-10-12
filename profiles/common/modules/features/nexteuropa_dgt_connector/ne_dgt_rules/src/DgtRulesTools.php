4c9b3f1a4dfad996efc2595433640f5fcf5a9908
voidtek <vitor.da-costa@ext.ec.europa.eu>
2017-10-12 20:09:37 +0200

    NEPT-1480: Add the subscriber with translation and review request.

--- profiles/common/modules/features/nexteuropa_dgt_connector/ne_dgt_rules/src/DgtRulesTools.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/ne_dgt_rules/src/DgtRulesTools.php
@@ -177,6 +177,252 @@ class DgtRulesTools {
   }
 
   /**
+   * Return related translations by the translated entity id.
+   *
+   * @param String $reference
+   *   Reference.
+   *
+   * @return array
+   *   An Array of TMGMTJob.
+   */
+  public static function loadTmgmtJobsByReference($reference) {
+    $job_ids = db_select('tmgmt_job', 'job')
+      ->fields('job', array('tjid'))
+      ->condition('job.reference', $reference, 'LIKE')
+      ->execute()
+      ->fetchAllAssoc('tjid');
+
+    return tmgmt_job_load_multiple(array_keys($job_ids));
+  }
+
+  /**
+   * Return related translations by the translated entity id.
+   *
+   * @param TMGMTJob $job
+   *   The TMGMT job.
+   * @param \EC\Poetry\Messages\Components\Status $status
+   *   The status.
+   */
+  public static function updateStatusTmgmtJob($job, $status) {
+    $statusMap = array(
+      'SUS' => TMGMT_JOB_STATE_ACTIVE,
+      'ONG' => TMGMT_JOB_STATE_ACTIVE,
+      'LCK' => TMGMT_JOB_STATE_ACTIVE,
+      'EXE' => TMGMT_JOB_STATE_ACTIVE,
+      'REF' => TMGMT_JOB_STATE_ABORTED,
+      'CNL' => TMGMT_JOB_STATE_ABORTED,
+    );
+
+    if ($statusMap[$status->getCode()] != $job->getState()) {
+      $job->setState($statusMap[$status->getCode()]);
+
+      DgtRulesTools::addMessageTmgmtJob(
+        $job,
+        'Update the status from @old_status to @new_status by DGT. Message: @message',
+        array(
+          '@old_status' => $job->getState(),
+          '@new_status' => $statusMap[$status->getCode()],
+          '@message' => $status->getMessage(),
+        )
+      );
+    }
+  }
+
+  /**
+   * Return related translations by the translated entity id.
+   *
+   * @param TMGMTJob $job
+   *   The TMGMT job.
+   * @param String $content
+   *   The status.
+   *
+   * @return bool
+   *   The
+   */
+  public static function updateTranslationTmgmtJob($job, $content) {
+    $job_items = $job->getItems();
+
+    foreach ($job_items as $job_item) {
+      $controller = tmgmt_file_format_controller($job->getSetting('export_format'));
+
+      if (!$controller) {
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('Failed to find the good controller, import aborted.'),
+          array(),
+          'error'
+        );
+
+        return FALSE;
+      }
+
+      $content = DgtRulesTools::parseTranslationTmgmtJob($job, $job_item, $content);
+
+      // Validate the file.
+      $validated_job = $controller->validateImport($content);
+
+      if (!$validated_job) {
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('@language Failed to validate file, import aborted.'),
+          array('@language' => $job->target_language),
+          'error'
+        );
+
+        return FALSE;
+      }
+
+      if ($validated_job->tjid != $job->tjid) {
+        $uri = $validated_job->uri();
+        $label = $validated_job->label();
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('Import file is from job <a href="@url">@label</a>, import aborted.'),
+          array(
+            '@language' => $job->target_language,
+            '@url' => url($uri['path']),
+            '@label' => $label,
+          ),
+          'error'
+        );
+
+        return FALSE;
+      }
+
+      if ($job->isAborted()) {
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('The Job is aborted, import aborted'),
+          array(),
+          'error'
+        );
+
+        return FALSE;
+      }
+
+      try {
+        // Validation successful, start import.
+        $job->addTranslatedData($controller->import($content));
+
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('Successfully received the translation file.'),
+          array()
+        );
+      }
+      catch (Exception $e) {
+        DgtRulesTools::addMessageTmgmtJob(
+          $job,
+          t('File import failed with the following message: @message'),
+          array('@message' => $e->getMessage()),
+          'error'
+        );
+      }
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * Return related translations by the translated entity id.
+   *
+   * @param TMGMTJob $job
+   *   The TMGMT job.
+   * @param String $content
+   *   The status.
+   *
+   * @return bool
+   *   The
+   */
+  public static function parseTranslationTmgmtJob($job, $job_item, $content) {
+    $translator = $job->getTranslator();
+
+    $dom = new \DOMDocument();
+    if (!multisite_drupal_toolbox_load_html($dom, base64_decode($content))) {
+      return FALSE;
+    }
+
+    // Workaround for saveXML() generating two xmlns attributes.
+    // See https://bugs.php.net/bug.php?id=47666.
+    if ($dom->documentElement->hasAttributeNS(NULL, 'xmlns')) {
+      $dom->documentElement->removeAttributeNS(NULL, 'xmlns');
+    }
+
+    $xml = simplexml_import_dom($dom);
+
+    if (count($xml->head->meta) > 0) {
+      foreach ($xml->head->meta as $meta_tag) {
+        if (isset($meta_tag['name'])) {
+          switch ($meta_tag['name']) {
+            case 'JobID':
+              $meta_tag['content'] = $job->tjid;
+              break;
+
+            case 'languageSource':
+              $meta_tag['content'] = $translator->mapToRemoteLanguage($job->source_language);
+              break;
+
+            case 'languageTarget':
+              $meta_tag['content'] = $translator->mapToRemoteLanguage($job->target_language);
+              break;
+          }
+        }
+      }
+    }
+    if (isset($xml->head->title)) {
+      $xml->head->title = "Job ID " . $job->tjid;
+    }
+    foreach ($xml->body->div as $parent_div) {
+      if ($parent_div['class'] == 'meta' && $parent_div['id'] == 'languageTarget') {
+        $parent_div[0] = $job->target_language;
+      }
+      if ($parent_div['class'] == 'asset') {
+        $parent_div['id'] = $job_item->tjiid;
+
+        /** @var SimpleXMLElement $div */
+        foreach ($parent_div->div as $div) {
+          if ($div['class'] == 'atom') {
+            $data = substr($div['id'], 1);
+            $data = base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+            $data = explode(']', $data);
+            $data[0] = $job_item->tjiid;
+            $data = implode(']', $data);
+            $div['id'] = 'b' . rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+          }
+        }
+      }
+    }
+
+    return $xml->saveXML();
+  }
+
+  /**
+   * Return related translations by the translated entity id.
+   *
+   * @param TMGMTJob $job
+   *   The TMGMT job.
+   * @param String $message
+   *   The message.
+   * @param array $array
+   *   The Array of variables for the message.
+   * @param String $type
+   *   The Array of variables for the message.
+   */
+  public static function addMessageTmgmtJob($job, $message, $array, $type = 'info') {
+    $job->addMessage($message, $array, $type);
+
+    $message = 'Job @reference-@language: ' . $message;
+    $array = array_merge(
+      $array,
+      array(
+        '@reference' => $job->reference,
+        '@language' => $job->target_language,
+      )
+    );
+    watchdog('ne_dgt_rules', $message, $array, WATCHDOG_INFO);
+  }
+
+  /**
    * Sends the review request to DGT Services for a given node.
    *
    * @param TMGMTJob $job
---
+++ profiles/common/modules/features/nexteuropa_dgt_connector/ne_tmgmt_dgt_ftt_translator/src/TMGMTDefaultSubscriber/TmgmtDgtFttSubscriber.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * @file
+ * Provides Next Europa TMGMT DGT FTT translator listener.
+ */
+
+namespace Drupal\ne_tmgmt_dgt_ftt_translator\TMGMTDefaultSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use EC\Poetry\Events\Notifications\TranslationReceivedEvent;
+use EC\Poetry\Events\Notifications\StatusUpdatedEvent;
+use Drupal\ne_dgt_rules\DgtRulesTools;
+use TMGMTJob;
+
+/**
+ * TMGMT DGT FTT translator listener.
+ */
+class TMGMTDgtFttSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Implements the event getSubscribedEvents.
+   */
+  public static function getSubscribedEvents() {
+    return array(
+      TranslationReceivedEvent::NAME => 'onTranslationReceivedEvent',
+      StatusUpdatedEvent::NAME  => 'onStatusUpdatedEvent',
+    );
+  }
+
+  /**
+   * Implements the event onTranslationReceivedEvent.
+   *
+   * @param TranslationReceivedEvent $event
+   *   The event for the translation Received.
+   */
+  public function onTranslationReceivedEvent(TranslationReceivedEvent $event) {
+    /** @var \EC\Poetry\Messages\Notifications\StatusUpdated $message */
+    $message = $event->getMessage();
+    $identifier = $message->getIdentifier();
+    $requestStatus = $message->getRequestStatus();
+
+    if ($requestStatus->getCode() != '0') {
+      watchdog(
+        'ne_dtmgmt_dgt_ftt_translator',
+        'Job @reference receives a Status Update with issues. Message: @message',
+        array(
+          '@reference' => $identifier->getFormattedIdentifier(),
+          '@message' => $requestStatus->getMessage(),
+        ),
+        WATCHDOG_ERROR
+      );
+
+      return;
+    }
+
+    watchdog(
+      'ne_dtmgmt_dgt_ftt_translator',
+      'Job @reference receives a Status Update.',
+      array('@reference' => $identifier->getFormattedIdentifier()),
+      WATCHDOG_INFO
+    );
+
+    $jobs = DgtRulesTools::loadTmgmtJobsByReference($identifier->getFormattedIdentifier());
+    $attributions = $message->getTargets();
+
+    /** @var \EC\Poetry\Messages\Components\Target $attribution */
+    foreach ($attributions as $attribution) {
+      /** @var TMGMTJob $job */
+      foreach ($jobs as $job) {
+        $translator = $job->getTranslator();
+        $job_language = strtoupper($translator->mapToRemoteLanguage($job->target_language));
+
+        if ($job_language == $attribution->getLanguage()) {
+          if (DgtRulesTools::UpdateTranslationTmgmtJob($job, $attribution->getTranslatedFile())) {
+            $job->acceptTranslation();
+
+            DgtRulesTools::addMessageTmgmtJob(
+              $job,
+              t('The translation has been accepted automatically by DGT.'),
+              array()
+            );
+          }
+
+          continue;
+        }
+      }
+    }
+  }
+
+  /**
+   * Implements the event onStatusUpdatedEvent.
+   *
+   * @param StatusUpdatedEvent $event
+   *   The event for the Status Update.
+   */
+  public function onStatusUpdatedEvent(StatusUpdatedEvent $event) {
+    /** @var \EC\Poetry\Messages\Notifications\StatusUpdated $message */
+    $message = $event->getMessage();
+    $identifier = $message->getIdentifier();
+
+    $jobs = DgtRulesTools::loadTmgmtJobsByReference($identifier->getFormattedIdentifier());
+    $requestStatus = $message->getRequestStatus();
+    $demandStatus = $message->getDemandStatus();
+    $attributionsStatuses = $message->getAttributionStatuses();
+
+    if ($requestStatus->getCode() != '0') {
+      watchdog(
+        'ne_dtmgmt_dgt_ftt_translator',
+        'Job @reference receives a Status Update with issues. Message: @message',
+        array(
+          '@reference' => $identifier->getFormattedIdentifier(),
+          '@message' => $requestStatus->getMessage(),
+        ),
+        WATCHDOG_ERROR
+      );
+
+      return;
+    }
+
+    watchdog(
+      'ne_dtmgmt_dgt_ftt_translator',
+      'Job @reference receives a Status Update.',
+      array('@reference' => $identifier->getFormattedIdentifier()),
+      WATCHDOG_INFO
+    );
+
+    // Checking the Demand Status.
+    foreach ($jobs as $job) {
+      DgtRulesTools::updateStatusTmgmtJob($job, $demandStatus);
+    }
+
+    /** @var \EC\Poetry\Messages\Components\Status $attributionStatus */
+    foreach ($attributionsStatuses as $attributionStatus) {
+      /** @var TMGMTJob $job */
+      foreach ($jobs as $job) {
+        $translator = $job->getTranslator();
+        $job_language = strtoupper($translator->mapToRemoteLanguage($job->target_language));
+
+        if ($job_language == $attributionStatus->getLanguage()) {
+          DgtRulesTools::UpdateStatusTmgmtJob($job, $attributionStatus);
+          continue;
+        }
+      }
+    }
+  }
+
+}
