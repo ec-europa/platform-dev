fe21174494c7c6e0016b77720725d97c61992e83
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-10-13 09:18:19 +0200

    NEPT-1563: Fix url variable name that was change in the youtube template.

--- profiles/common/modules/features/ec_embedded_video/themes/media-youtube-video.tpl.php
+++ profiles/common/modules/features/ec_embedded_video/themes/media-youtube-video.tpl.php
@@ -22,7 +22,7 @@
 <?php if (!$no_wrapper): ?>
   <div class="<?php print $classes; ?> media-youtube-<?php print $id; ?>">
 <?php endif ?>
-  <iframe class="media-youtube-player" <?php print $api_id_attribute; ?>width="<?php print $width; ?>" height="<?php print $height; ?>" title="<?php print $title; ?>" src="<?php print $url; ?>" frameborder="0" allowfullscreen><?php print $alternative_content; ?></iframe>
+  <iframe class="media-youtube-player" <?php print $api_id_attribute; ?>width="<?php print $width; ?>" height="<?php print $height; ?>" title="<?php print $title; ?>" src="<?php print $ec_embedded_video_url; ?>" frameborder="0" allowfullscreen><?php print $alternative_content; ?></iframe>
 <?php if (!$no_wrapper): ?>
   </div>
 <?php endif ?>
