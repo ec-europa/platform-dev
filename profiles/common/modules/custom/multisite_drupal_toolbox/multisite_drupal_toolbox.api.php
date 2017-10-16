833dcf0431164c91a9113ca587e03aea064f1d3e
dvargas <hiphipvargas@sapo.pt>
2017-10-16 15:42:21 +0200

    NEPT-1494: Improve minor details after code review (comments, new line in the end of file...)

--- profiles/common/modules/custom/multisite_drupal_toolbox/multisite_drupal_toolbox.api.php
+++ profiles/common/modules/custom/multisite_drupal_toolbox/multisite_drupal_toolbox.api.php
@@ -9,6 +9,6 @@
  * Alter the default filter options for the Sanitize HTML text filter.
  */
 function hook_multisite_drupal_toolbox_filter_options_alter(&$filter_options) {
-  // Enable the <iframe> tag, strongly not advised of course.
-  $filter_options['valid_elements']['iframe'] = array('*' => TRUE);
+  // Enable the <object> tag, strongly not advised of course.
+  $filter_options['valid_elements']['object'] = array('*' => TRUE);
 }
