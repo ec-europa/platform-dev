cdfe7d954e5e7b8e3325ec7caf188db234f39fe7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2017-10-16 10:44:30 +0200

    NEPT-1494: Sanitize HTML filter: Remove the central configuration feature & the node related parameter.

---
+++ profiles/common/modules/custom/multisite_drupal_toolbox/multisite_drupal_toolbox.api.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Hooks for multisite_drupal_toolbox module.
+ */
+
+/**
+ * Alter the default filter options for the Sanitize HTML text filter.
+ */
+function hook_multisite_drupal_toolbox_filter_options_alter(&$filter_options) {
+  // Enable the <iframe> tag, strongly not advised of course.
+  $filter_options['valid_elements']['iframe'] = array('*' => TRUE);
+}
