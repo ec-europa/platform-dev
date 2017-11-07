e48a88d0722c6d053b77f46e261ef0eb5ef70259
dvargas <hiphipvargas@sapo.pt>
2017-11-07 03:06:21 +0100

    NEPT-1519: End POC with working behat test and ready for demo (working for second languages)

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/src/Subscriber.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/src/Subscriber.php
@@ -43,74 +43,167 @@ class Subscriber implements EventSubscriberInterface {
     $message = $event->getMessage();
     $reference = $message->getIdentifier()->getFormattedIdentifier();
 
-    // Get main job in order to register the messages.
+    // Get main job in order to register the messages and get translator.
     $main_reference = 'MAIN_%_POETRY_%' . $reference;
     $targets = $message->getTargets();
-
     /** @var \EC\Poetry\Messages\Components\Target $target */
-    foreach ($targets as $target) {
-      $language_job = [$translator->mapToLocalLanguage(drupal_strtolower($target->getLanguage()))];
+    $target = array_shift($targets);
 
-      $ids = _tmgmt_poetry_obtain_related_translation_jobs(
-        $language_job,
-        $main_reference
-      )->fetchAll();
-      if (!$ids) {
-        watchdog(
-          "tmgmt_poetry",
-          "Callback can't find !language for job with remote reference !reference .",
-          ['!language' => $target->getLanguage(), '!reference' => $reference],
-          WATCHDOG_ERROR
-        );
-        return FALSE;
-      }
+    $ids = _tmgmt_poetry_obtain_related_translation_jobs(
+      array(),
+      $main_reference
+    )->fetchAll();
+    if (empty($ids)) {
+      watchdog(
+        "tmgmt_poetry",
+        "Callback can't find job with reference !reference .",
+        array('!reference' => $main_reference),
+        WATCHDOG_ERROR
+      );
+      return FALSE;
+    }
 
-      // Get right controller from main job.
-      $main_id = $ids[0];
-      $main_job = tmgmt_job_load($main_id->tjid);
-      if ($main_job->isAborted()) {
-        return FALSE;
-      }
-      $controller = tmgmt_file_format_controller($main_job->getSetting('export_format'));
-      if (!$controller) {
-        return FALSE;
-      }
+    // Get right controller from main job.
+    $main_id = array_shift($ids);
+    $main_job = tmgmt_job_load($main_id->tjid);
+    if ($main_job->isAborted()) {
+      return FALSE;
+    }
+    $controller = tmgmt_file_format_controller($main_job->getSetting('export_format'));
+    if (!$controller) {
+      return FALSE;
+    }
 
-      // Import content using controller.
-      $imported_file = base64_decode($target->getTranslatedFile());
-      $content = $controller->import($imported_file);
+    // Get main job.
+    $language_job = $translator->mapToLocalLanguage(drupal_strtolower($target->getLanguage()));
+    $ids = _tmgmt_poetry_obtain_related_translation_jobs(array($language_job), $reference)
+      ->fetchAll();
+    $main_ids = $ids[0];
+    $job = tmgmt_job_load($main_ids->tjid);
+    $job_item = tmgmt_job_item_load($main_ids->tjiid);
 
-      foreach ($ids as $id) {
+    // Import content using controller.
+    $imported_file = base64_decode($target->getTranslatedFile());
+    if ($language_job != $main_job->target_language) {
+      $imported_file = $this->tmgmtPoetryRewriteReceivedXml($imported_file, $job, $ids);
+    }
+    $content = $controller->import($imported_file);
 
-        $job = tmgmt_job_load($id->tjid);
-        $job_item = tmgmt_job_item_load($id->tjiid);
+    try {
+      // Validation successful, start import.
+      $job->addTranslatedData($content);
 
-        try {
-          // Validation successful, start import.
-          $job->addTranslatedData($content);
+      $main_job->addMessage(
+        t('@language Successfully received the translation file.'),
+        ['@language' => $job->target_language]
+      );
 
-          $job->addMessage(
-            t('@language Successfully received the translation file.'),
-            ['@language' => $job->target_language]
-          );
+      // Update the status to executed when we receive a translation.
+      _tmgmt_poetry_update_item_status($job_item->tjiid, "", "Executed", "");
 
-          // Update the status to executed when we receive a translation.
-          _tmgmt_poetry_update_item_status($job_item->tjiid, "", "Executed", "");
+    }
+    catch (Exception $e) {
 
-        }
-        catch (Exception $e) {
-          $job->addMessage(
-            t('@language File import failed with the following message: @message'),
-            [
-              '@language' => $job->target_language,
-              '@message' => $e->getMessage(),
-            ],
-            'error'
-          );
-          watchdog_exception('tmgmt_poetry', $e);
+      $main_job->addMessage(
+        t('@language File import failed with the following message: @message'),
+        [
+          '@language' => $job->target_language,
+          '@message' => $e->getMessage(),
+        ],
+        'error'
+      );
+      watchdog_exception('tmgmt_poetry', $e);
+    }
+  }
+
+  /**
+   * Replace job id in received content.
+   *
+   * @param string $content
+   *   The XML content.
+   * @param \TMGMTJob $job
+   *   The job.
+   * @param array $ids_collection
+   *   The array of pairs with jobs and job items.
+   *
+   * @return bool|mixed
+   *   The updated XML content.
+   */
+  private function tmgmtPoetryRewriteReceivedXml($content, $job, $ids_collection) {
+
+    $dom = new \DOMDocument();
+    if (!multisite_drupal_toolbox_load_html($dom, $content)) {
+      return FALSE;
+    }
+
+    // Workaround for saveXML() generating two xmlns attributes.
+    // See https://bugs.php.net/bug.php?id=47666.
+    if ($dom->documentElement->hasAttributeNS(NULL, 'xmlns')) {
+      $dom->documentElement->removeAttributeNS(NULL, 'xmlns');
+    }
+
+    $xml = simplexml_import_dom($dom);
+
+    if (count($xml->head->meta) > 0) {
+      foreach ($xml->head->meta as $meta_tag) {
+        if (isset($meta_tag['name'])) {
+          switch ($meta_tag['name']) {
+            case 'JobID':
+              $meta_tag['content'] = $job->tjid;
+              break;
+
+            case 'languageSource':
+              $meta_tag['content'] = $job->getTranslator()
+                ->mapToRemoteLanguage($job->source_language);
+              break;
+
+            case 'languageTarget':
+              $meta_tag['content'] = $job->getTranslator()
+                ->mapToRemoteLanguage($job->target_language);
+              break;
+          }
         }
       }
     }
+    if (isset($xml->head->title)) {
+      $xml->head->title = "Job ID " . $job->tjid;
+    }
+    foreach ($xml->body->div as $parent_div) {
+      if ($parent_div['class'] == 'meta' && $parent_div['id'] == 'languageTarget') {
+        $parent_div[0] = $job->target_language;
+      }
+      if ($parent_div['class'] == 'asset') {
+
+        /** @var SimpleXMLElement $div */
+        foreach ($parent_div->div as $div) {
+          if ($div['class'] == 'atom') {
+            $data = substr($div['id'], 1);
+            $data = base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+            $data = explode(']', $data);
+            $main_tjiid = $data[0];
+            // This is the main job item for main job.
+            $main_job_item = tmgmt_job_item_load($main_tjiid);
+
+            $corresponding_tjiid = 0;
+            foreach ($ids_collection as $ids) {
+              $job_item_to_test = tmgmt_job_item_load($ids->tjiid);
+              if ($job_item_to_test->item_id == $main_job_item->item_id && $job_item_to_test->item_type == $main_job_item->item_type) {
+                // This is the corresponding job item.
+                $corresponding_tjiid = $ids->tjiid;
+                continue;
+              }
+            }
+
+            $data[0] = $corresponding_tjiid;
+            $data = implode(']', $data);
+            $div['id'] = 'b' . rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+          }
+        }
+      }
+    }
+
+    $result = $xml->saveXML();
+    return $result;
   }
 
 
--- tests/features/tmgmt/tmgmt_dgt_connector_client.feature
+++ tests/features/tmgmt/tmgmt_dgt_connector_client.feature
@@ -144,6 +144,31 @@ Feature: TMGMT Poetry features
     """
     And I reload the page
     Then I should see "Needs review" in the "French" row
-    And I click "Needs review" in the "French" row
+    When I click "Needs review" in the "French" row
     And I press "Save as completed"
     Then I should see "None" in the "French" row
+    And I should see "My page 1 FR" in the "French" row
+    When Poetry notifies the client with the following XML:
+    """
+    <POETRY xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="">
+      <request communication="synchrone" id="3558615" type="translation">
+        <demandeId>
+           <codeDemandeur>WEB</codeDemandeur>
+           <annee>2017</annee>
+           <numero>1234</numero>
+           <version>0</version>
+           <partie>0</partie>
+           <produit>TRA</produit>
+        </demandeId>
+        <attributions format="HTML" lgCode="ES">
+           <attributionsFile>PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XM0MvL0RURCBYSFRNTCAxLjAgU3RyaWN0Ly9FTiIgImh0dHA6Ly93d3cudzMub3JnL1RSL3hodG1sMS9EVEQveGh0bWwxLXN0cmljdC5kdGQiPgo8aHRtbCB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94aHRtbCI+CiAgPGhlYWQ+CiAgICA8bWV0YSBodHRwLWVxdWl2PSJDb250ZW50LVR5cGUiIGNvbnRlbnQ9InRleHQvaHRtbDsgY2hhcnNldD1VVEYtOCIgLz4KICAgIDxtZXRhIG5hbWU9IkpvYklEIiBjb250ZW50PSIxIiAvPgogICAgPG1ldGEgbmFtZT0ibGFuZ3VhZ2VTb3VyY2UiIGNvbnRlbnQ9ImVuIiAvPgogICAgPG1ldGEgbmFtZT0ibGFuZ3VhZ2VUYXJnZXQiIGNvbnRlbnQ9ImVzIiAvPgogICAgPHRpdGxlPkpvYiBJRCAxPC90aXRsZT4KICA8L2hlYWQ+CiAgPGJvZHk+CiAgICAgICAgICA8ZGl2IGNsYXNzPSJhc3NldCIgaWQ9Iml0ZW0tMyI+CiAgICAgICAgICAgICAgICAgICAgICAgICAgPCEtLQogICAgICAgICAgbGFiZWw9IlRpdGxlIgogICAgICAgICAgY29udGV4dD0iWzNdW3RpdGxlX2ZpZWxkXVswXVt2YWx1ZV0iCiAgICAgICAgLS0+CiAgICAgICAgPGRpdiBjbGFzcz0iYXRvbSIgaWQ9ImJNMTFiZEdsMGJHVmZabWxsYkdSZFd6QmRXM1poYkhWbCI+TXkgcGFnZSAxIEVTPC9kaXY+CiAgICAgICAgICAgICAgICAgICAgICAgICAgPCEtLQogICAgICAgICAgbGFiZWw9IkJvZHkiCiAgICAgICAgICBjb250ZXh0PSJbM11bZmllbGRfbmVfYm9keV1bMF1bdmFsdWVdIgogICAgICAgIC0tPgogICAgICAgIDxkaXYgY2xhc3M9ImF0b20iIGlkPSJiTTExYlptbGxiR1JmYm1WZlltOWtlVjFiTUYxYmRtRnNkV1UiPlNob3J0IGJvZHkgMSBFUzwvZGl2PgogICAgICAgICAgICAgIDwvZGl2PgogICAgICAgICAgPGRpdiBjbGFzcz0iYXNzZXQiIGlkPSJpdGVtLTQiPgogICAgICAgICAgICAgICAgICAgICAgICAgIDwhLS0KICAgICAgICAgIGxhYmVsPSJUaXRsZSIKICAgICAgICAgIGNvbnRleHQ9Ils0XVt0aXRsZV9maWVsZF1bMF1bdmFsdWVdIgogICAgICAgIC0tPgogICAgICAgIDxkaXYgY2xhc3M9ImF0b20iIGlkPSJiTkYxYmRHbDBiR1ZmWm1sbGJHUmRXekJkVzNaaGJIVmwiPk15IHBhZ2UgMiBFUzwvZGl2PgogICAgICAgICAgICAgICAgICAgICAgICAgIDwhLS0KICAgICAgICAgIGxhYmVsPSJCb2R5IgogICAgICAgICAgY29udGV4dD0iWzRdW2ZpZWxkX25lX2JvZHldWzBdW3ZhbHVlXSIKICAgICAgICAtLT4KICAgICAgICA8ZGl2IGNsYXNzPSJhdG9tIiBpZD0iYk5GMWJabWxsYkdSZmJtVmZZbTlrZVYxYk1GMWJkbUZzZFdVIj5TaG9ydCBib2R5IDIgRVM8L2Rpdj4KICAgICAgICAgICAgICA8L2Rpdj4KICAgICAgPC9ib2R5Pgo8L2h0bWw+</attributionsFile>
+        </attributions>
+     </request>
+   </POETRY>
+    """
+    And I reload the page
+    Then I should see "Needs review" in the "Spanish" row
+    When I click "Needs review" in the "Spanish" row
+    And I press "Save as completed"
+    Then I should see "None" in the "Spanish" row
+    And I should see "My page 1 ES" in the "Spanish" row
