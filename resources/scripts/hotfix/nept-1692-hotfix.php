59c6585664b91eaf04189ac5012a48524fe7ee36
Yemoko <mbk@kuenza.com>
2017-12-19 13:10:56 +0100

    NEPT-1692: Add condition to avoid unwanted actions in the database.

--- resources/scripts/hotfix/nept-1692-hotfix.php
+++ resources/scripts/hotfix/nept-1692-hotfix.php
@@ -25,44 +25,54 @@ function my_module_update_xxxx() {
     ->fields('tji', array('tjiid'));
   $tjiids = $tjiid_list_query->execute()->fetchCol();
 
-  db_delete('poetry_status')->condition('tjiid', $tjiids, 'IN')->execute();
+  if (!empty($tjiids)) {
+    db_delete('poetry_status')->condition('tjiid', $tjiids, 'IN')->execute();
 
-  watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjiid have been deleted 
-  from "poetry_status" tables: @list.', array('@list' => implode(' ,', $tjiids)));
+    watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjiid have been deleted 
+  from "poetry_status" tables: @list.', ['@list' => implode(' ,', $tjiids)]);
 
-  // Clean "tmgmt_job_item" table.
-  $tjid_list_query = db_select('tmgmt_job', 'tj')
-    ->condition('reference', '%' . $implied_reference, 'LIKE')
-    ->fields('tj', array('tjid'));
-  $tjids = $tjid_list_query->execute()->fetchCol();
+    // Clean "tmgmt_job_item" table.
+    $tjid_list_query = db_select('tmgmt_job', 'tj')
+      ->condition('reference', '%' . $implied_reference, 'LIKE')
+      ->fields('tj', ['tjid']);
+    $tjids = $tjid_list_query->execute()->fetchCol();
 
-  db_delete('tmgmt_job_item')->condition('tjid', $tjids, 'IN')->execute();
+    db_delete('tmgmt_job_item')->condition('tjid', $tjids, 'IN')->execute();
 
-  // Clean "tmgmt_message" table.
-  db_delete('tmgmt_message')->condition('tjid', $tjids, 'IN')->execute();
+    // Clean "tmgmt_message" table.
+    db_delete('tmgmt_message')->condition('tjid', $tjids, 'IN')->execute();
 
-  watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjid have been deleted 
-  from "tmgmt_job_item" and "tmgmt_message" tables: @list.', array('@list' => implode(' ,', $tjids)));
+    watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjid have been deleted 
+  from "tmgmt_job_item" and "tmgmt_message" tables: @list.', ['@list' => implode(' ,', $tjids)]);
 
-  // Clean "poetry_map" table.
-  db_delete('poetry_map')
-    ->condition('entity_type', 'node', '=')
-    ->condition('entity_id', $implied_nid, '=')
-    ->condition('annee', $implied_annee, '=')
-    ->condition('numero', $implied_numero, '=')
-    ->condition('version', $implied_version, '=')
-    ->condition('partie', $implied_partie, '=')->execute();
+    // Clean "poetry_map" table.
+    db_delete('poetry_map')
+      ->condition('entity_type', 'node', '=')
+      ->condition('entity_id', $implied_nid, '=')
+      ->condition('annee', $implied_annee, '=')
+      ->condition('numero', $implied_numero, '=')
+      ->condition('version', $implied_version, '=')
+      ->condition('partie', $implied_partie, '=')->execute();
 
-  watchdog('NextEuropa DGT Connector Cleaning', 'The row that concerns the node with the nid @nid and the "partie" @partie is deleted  
-  from "poetry_map" tables: @list.', array('@nid' => $implied_nid, '@partie' => $implied_partie));
+    watchdog('NextEuropa DGT Connector Cleaning', 'The row that concerns the node with the nid @nid and the "partie" @partie is deleted  
+  from "poetry_map" tables: @list.', [
+      '@nid' => $implied_nid,
+      '@partie' => $implied_partie
+    ]);
 
-  // Clean "tmgmt_job" table.
-  db_delete('tmgmt_job')->condition('reference', '%' . $implied_reference, 'LIKE')->execute();
+    // Clean "tmgmt_job" table.
+    db_delete('tmgmt_job')
+      ->condition('reference', '%' . $implied_reference, 'LIKE')
+      ->execute();
 
-  watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjid are supposed to be deleted 
-  from "tmgmt_job" table: @list.', array('@list' => implode(' ,', $tjids)));
+    watchdog('NextEuropa DGT Connector Cleaning', 'All rows related to one of these tjid are supposed to be deleted 
+  from "tmgmt_job" table: @list.', ['@list' => implode(' ,', $tjids)]);
 
-  return t(
-    'Poetry job clean done for the reference @ref and the node @nid, please check the watchdog logs for more information.',
-    array('@ref' => $implied_reference, '@nid' => $implied_nid));
+    return t(
+      'Poetry job clean done for the reference @ref and the node @nid, please check the watchdog logs for more information.',
+      ['@ref' => $implied_reference, '@nid' => $implied_nid]);
+  }
+  else{
+    return t('No action needed for Poetry job.');
+  }
 }
