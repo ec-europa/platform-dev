c27632ec0f4f6a052f9ae8875ea68ec991de2716
imanoleguskiza <ier@opendeusto.es>
2018-01-03 15:00:14 +0100

    NEPT-1665: Added custom functionality to add items to a cart.

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartBundle.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartBundle.php
@@ -3,6 +3,7 @@
 namespace Drupal\tmgmt_dgt_connector_cart\Entity;
 
 use Entity;
+use EntityFieldQuery;
 
 /**
  * DGT FTT Translator mapping entity.
@@ -22,6 +23,57 @@ class CartBundle extends Entity {
 
     $return = parent::save();
     return $return;
+  }
+
+  /**
+   * Load a cart bundle entity from the database.
+   */
+  public static function load($cbid, $reset) {
+    $cbids = (isset($cbid) ? array($cbid) : array());
+    $cart_bundle = self::loadMultiple($cbids, $reset);
+    return $cart_bundle ? reset($cart_bundle) : FALSE;
+  }
+
+  /**
+   * Load cart bundle entities from the database.
+   */
+  public static function loadMultiple($cbids, $reset = FALSE) {
+    return entity_load('cart_bundle', $cbids, array(), $reset);
+  }
+
+  /**
+   * Load cart bundle entities filtered by a set of properties.
+   */
+  public static function loadWithProperties($properties) {
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'cart_bundle');
+    foreach ($properties as $property => $value) {
+      $query->propertyCondition($property, $value);
+    }
+
+    $result = $query->execute();
+    if (isset($result['cart_bundle'])) {
+      $cbids = array_keys($result['cart_bundle']);
+      return self::loadMultiple($cbids);
+    }
+    return array();
+  }
+
+  /**
+   * Create a cart bundle entity.
+   */
+  public static function create($uid, $target_languages, $status, $tjid = 0) {
+    $cart_bundle = entity_create(
+      'cart_bundle',
+      array(
+        'uid' => $uid,
+        'tjid' => $tjid,
+        'target_languages' => $target_languages,
+        'status' => $status,
+      )
+    );
+    $cart_bundle->save();
+    return $cart_bundle;
   }
 
 }
--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartItem.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartItem.php
@@ -3,6 +3,7 @@
 namespace Drupal\tmgmt_dgt_connector_cart\Entity;
 
 use Entity;
+use EntityFieldQuery;
 
 /**
  * DGT FTT Translator mapping entity.
@@ -22,6 +23,59 @@ class CartItem extends Entity {
 
     $return = parent::save();
     return $return;
+  }
+
+  /**
+   * Load a cart item entity from the database.
+   */
+  public static function load($ciid, $reset) {
+    $ciids = (isset($ciid) ? array($ciid) : array());
+    $cart_bundle = self::loadMultiple($ciids, $reset);
+    return $cart_bundle ? reset($cart_bundle) : FALSE;
+  }
+
+  /**
+   * Load cart item entities from the database.
+   */
+  public static function loadMultiple($ciids, $reset = FALSE) {
+    return entity_load('cart_item', $ciids, array(), $reset);
+  }
+
+  /**
+   * Load cart item entities filtered by a set of properties.
+   */
+  public static function loadWithProperties($properties) {
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'cart_item');
+    foreach ($properties as $property => $value) {
+      $query->propertyCondition($property, $value);
+    }
+
+    $result = $query->execute();
+    if (isset($result['cart_item'])) {
+      $cbids = array_keys($result['cart_item']);
+      return self::loadMultiple($cbids);
+    }
+    return array();
+  }
+
+  /**
+   * Create a cart item entity.
+   */
+  public static function create($cbid, $entity_type, $entity_id, $context_url = '', $context_comment = '', $tjiid = 0) {
+    $cart_item = entity_create(
+      'cart_item',
+      array(
+        'cbid' => $cbid,
+        'entity_type' => $entity_type,
+        'entity_id' => $entity_id,
+        'context_url' => $context_url,
+        'context_comment' => $context_comment,
+        'tjiid' => $tjiid,
+      )
+    );
+    $cart_item->save();
+    return $cart_item;
   }
 
 }
