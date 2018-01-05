1b862490aaa98f7d774c6e048c50fcb3b2e92cbd
netlooker <czarny@data.pl>
2018-01-05 14:28:24 +0100

    NEPT-1665: Small improvements done during the first review.

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartBundle.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartBundle.php
@@ -9,6 +9,10 @@ use EntityFieldQuery;
  * DGT FTT Translator mapping entity.
  */
 class CartBundle extends Entity {
+  const STATUS_OPEN = 'OPEN';
+  const STATUS_SEND = 'SEND';
+  const STATUS_FINISHED = 'FINISHED';
+  const STATUS_DISCARDED = 'DISCARDED';
 
   /**
    * Override the save to update date properties.
@@ -18,15 +22,21 @@ class CartBundle extends Entity {
     if (empty($this->created)) {
       $this->created = REQUEST_TIME;
     }
-
     $this->changed = REQUEST_TIME;
 
-    $return = parent::save();
-    return $return;
+    return parent::save();
   }
 
   /**
    * Load a cart bundle entity from the database.
+   *
+   * @param int $cbid
+   *   The CartBundle entity ID.
+   * @param bool $reset
+   *   Optional reset of the internal cache for the requested entity type.
+   *
+   * @return bool|CartBundle
+   *   The CartBundle entity or FALSE if the entity was not found.
    */
   public static function load($cbid, $reset) {
     $cbids = (isset($cbid) ? array($cbid) : array());
@@ -35,32 +45,62 @@ class CartBundle extends Entity {
   }
 
   /**
-   * Load cart bundle entities from the database.
+   * Load CartBundle entities from the database.
+   *
+   * @param array $cbids
+   *   An array of CartBundle entities IDs.
+   * @param bool $reset
+   *   Optional reset of the internal cache for the requested entity type.
+   *
+   * @return array
+   *   An array of CartBundle entity objects indexed by their ids or an empty
+   *   array if no results are found.
    */
-  public static function loadMultiple($cbids, $reset = FALSE) {
+  public static function loadMultiple(array $cbids, $reset = FALSE) {
     return entity_load('cart_bundle', $cbids, array(), $reset);
   }
 
   /**
-   * Load cart bundle entities filtered by a set of properties.
+   * Load CartBundle entities filtered by a set of properties.
+   *
+   * @param array $properties
+   *   An array with properties for querying for specific entities.
+   *
+   * @return array
+   *   An array of CartBundle entity objects indexed by their IDs or an empty
+   *   array if no results are found.
    */
-  public static function loadWithProperties($properties) {
+  public static function loadWithProperties(array $properties) {
     $query = new EntityFieldQuery();
     $query->entityCondition('entity_type', 'cart_bundle');
     foreach ($properties as $property => $value) {
       $query->propertyCondition($property, $value);
     }
-
     $result = $query->execute();
+
     if (isset($result['cart_bundle'])) {
       $cbids = array_keys($result['cart_bundle']);
+
       return self::loadMultiple($cbids);
     }
+
     return array();
   }
 
   /**
-   * Create a cart bundle entity.
+   * Create a CartBundle entity.
+   *
+   * @param int $uid
+   *   An ID of the currently logged in user.
+   * @param string $target_languages
+   *   String with concatenated language prefixes.
+   * @param string $status
+   *   A cart bundle status.
+   * @param int $tjid
+   *   The TMGMTJob entity ID.
+   *
+   * @return bool
+   *   A new instance of the CartBundle entity or FALSE.
    */
   public static function create($uid, $target_languages, $status, $tjid = 0) {
     $cart_bundle = entity_create(
@@ -73,6 +113,7 @@ class CartBundle extends Entity {
       )
     );
     $cart_bundle->save();
+
     return $cart_bundle;
   }
 
--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartItem.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/Entity/CartItem.php
@@ -14,53 +14,94 @@ class CartItem extends Entity {
    * Override the save to update date properties.
    */
   public function save() {
-
     if (empty($this->created)) {
       $this->created = REQUEST_TIME;
     }
-
     $this->changed = REQUEST_TIME;
 
-    $return = parent::save();
-    return $return;
+    return parent::save();
   }
 
   /**
    * Load a cart item entity from the database.
+   *
+   * @param int $ciid
+   *   The CartItem entity ID.
+   * @param bool $reset
+   *   Optional reset of the internal cache for the requested entity type.
+   *
+   * @return array
+   *   An array of CartBundle entity objects indexed by their ids or an empty
+   *   array if no results are found.
    */
   public static function load($ciid, $reset) {
     $ciids = (isset($ciid) ? array($ciid) : array());
     $cart_bundle = self::loadMultiple($ciids, $reset);
+
     return $cart_bundle ? reset($cart_bundle) : FALSE;
   }
 
   /**
-   * Load cart item entities from the database.
+   * Load CartItem entities from the database.
+   *
+   * @param array $ciids
+   *   An array of CartItem entities IDs.
+   * @param bool $reset
+   *   Optional reset of the internal cache for the requested entity type.
+   *
+   * @return array
+   *   An array of CartItems entity objects indexed by their ids or an empty
+   *   array if no results are found.
    */
-  public static function loadMultiple($ciids, $reset = FALSE) {
+  public static function loadMultiple(array $ciids, $reset = FALSE) {
     return entity_load('cart_item', $ciids, array(), $reset);
   }
 
   /**
-   * Load cart item entities filtered by a set of properties.
+   * Load CartItem entities filtered by a set of properties.
+   *
+   * @param array $properties
+   *   An array with properties for querying for specific entities.
+   *
+   * @return array
+   *   An array of CartItem entity objects indexed by their IDs or an empty
+   *   array if no results are found.
    */
-  public static function loadWithProperties($properties) {
+  public static function loadWithProperties(array $properties) {
     $query = new EntityFieldQuery();
     $query->entityCondition('entity_type', 'cart_item');
     foreach ($properties as $property => $value) {
       $query->propertyCondition($property, $value);
     }
-
     $result = $query->execute();
+
     if (isset($result['cart_item'])) {
       $cbids = array_keys($result['cart_item']);
+
       return self::loadMultiple($cbids);
     }
+
     return array();
   }
 
   /**
-   * Create a cart item entity.
+   * Create a CartItem entity.
+   *
+   * @param int $cbid
+   *   The CartBundle entity ID.
+   * @param string $entity_type
+   *   An entity type.
+   * @param string $entity_id
+   *   An entity ID.
+   * @param string $context_url
+   *   A context URL.
+   * @param string $context_comment
+   *   A context comment.
+   * @param int $tjiid
+   *   The TMGMTJobItem entity ID.
+   *
+   * @return bool
+   *   A new instance of the CartItem entity or FALSE.
    */
   public static function create($cbid, $entity_type, $entity_id, $context_url = '', $context_comment = '', $tjiid = 0) {
     $cart_item = entity_create(
@@ -75,6 +116,7 @@ class CartItem extends Entity {
       )
     );
     $cart_item->save();
+
     return $cart_item;
   }
 
