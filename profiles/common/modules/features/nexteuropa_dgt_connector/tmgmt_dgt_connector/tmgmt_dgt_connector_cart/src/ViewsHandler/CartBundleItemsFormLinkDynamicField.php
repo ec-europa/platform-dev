9ad424408649835073e89b47456125b5c98703ac
netlooker <czarny@data.pl>
2018-01-10 11:49:33 +0100

    NEPT-1666: Implementation of the CartBundles View + placeholder for the CartItems management + improvements

---
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/ViewsHandler/CartBundleItemsFormLinkDynamicField.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\tmgmt_dgt_connector_cart\ViewsHandler;
+
+use views_handler_field;
+
+/**
+ * Custom Views field handler to present a target languages field.
+ */
+class CartBundleItemsFormLinkDynamicField extends views_handler_field {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render($values) {
+    ctools_include('modal');
+    ctools_modal_add_js();
+    $cbid = $this->get_value($values);
+    $link_text = 'edit';
+    return '<div id="magical-modal-link">' .
+      l(
+        $link_text,
+        "admin/dgt_connector/cart-items-wrapper/$cbid/nojs",
+        array(
+          'attributes' => array(
+            'class' => 'ctools-use-modal',
+          ),
+        )
+      )
+    . '</div>';
+  }
+
+}
