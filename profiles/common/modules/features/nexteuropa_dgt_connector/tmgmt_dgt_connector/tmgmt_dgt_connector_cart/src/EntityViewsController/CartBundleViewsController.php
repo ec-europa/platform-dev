431486fedf6c6830a1f3fd1d7779c5baf291705c
netlooker <czarny@data.pl>
2018-01-16 17:56:55 +0100

    NEPT-1666: Implementation of the CartItems management form and some tweaks.

--- profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/EntityViewsController/CartBundleViewsController.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/tmgmt_dgt_connector/tmgmt_dgt_connector_cart/src/EntityViewsController/CartBundleViewsController.php
@@ -22,8 +22,17 @@ class CartBundleViewsController extends EntityDefaultViewsController {
     // Injecting the custom handler for the target languages.
     $data['cart_bundle']['target_languages']['field']['handler'] = 'Drupal\\tmgmt_dgt_connector_cart\\ViewsHandler\\CartBundleField';
 
+    $data['cart_bundle']['cart_bundle_items_titles'] = array(
+      'title' => t('Bundle items titles'),
+      'help' => t('The titles list of related CartItems entities.'),
+      'real field' => 'cbid',
+      'field' => array(
+        'handler' => 'Drupal\\tmgmt_dgt_connector_cart\\ViewsHandler\\CartBundleItemsTitlesDynamicField',
+      ),
+    );
+
     $data['cart_bundle']['cart_bundle_items_form_link'] = array(
-      'title' => t('Bundle items'),
+      'title' => t('Bundle items link'),
       'help' => t('The list of CartItems entities for a given CartBundle.'),
       'real field' => 'cbid',
       'field' => array(
