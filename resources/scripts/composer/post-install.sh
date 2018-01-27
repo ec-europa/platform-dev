1a5def9a80e6cb74da6e004b78dc74a7a9e03c51
Alex Verbruggen <verbruggenalex@gmail.com>
2018-01-27 09:57:01 -0800

    Update post install

--- resources/scripts/composer/post-install.sh
+++ resources/scripts/composer/post-install.sh
@@ -10,3 +10,14 @@ ln -s "../../vendor/pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
 # See https://github.com/wimg/PHPCompatibility/issues/102.
 rm -rf vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
 cp -r vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility;
+
+# Execute the drush-m2c to regenerate the composer.json files.
+for profile in multisite_drupal_standard multisite_drupal_communities; do
+  cd profiles/$profile
+  ../../bin/drush m2c \
+    $profile.make \
+    composer.json \
+    --require-dev \
+    --custom=modules/custom,modules/features
+  cd ../..
+done
