911f62f9b656dece36bfb040de20533774a32de1
netlooker <czarny@data.pl>
2018-02-15 14:47:48 +0100

    NEPT-1835: Extension of an action to send the direct translations + Behat test

--- profiles/common/modules/features/nexteuropa_dgt_connector/ne_tmgmt_dgt_ftt_translator/src/TMGMTDefaultTranslatorPluginController/TmgmtDgtFttTranslatorPluginController.php
+++ profiles/common/modules/features/nexteuropa_dgt_connector/ne_tmgmt_dgt_ftt_translator/src/TMGMTDefaultTranslatorPluginController/TmgmtDgtFttTranslatorPluginController.php
@@ -224,6 +224,58 @@ class TmgmtDgtFttTranslatorPluginController extends TMGMTDefaultTranslatorPlugin
   }
 
   /**
+   * Sends the direct translation request to the DGT Service.
+   *
+   * @param array $jobs
+   *   Array of TMGMT Job object.
+   * @param array $parameters
+   *   An array with additional parameters like the organisation data.
+   *
+   * @return array|bool
+   *   An array with data for the 'Rules workflow' or FALSE if errors appear.
+   */
+  public function requestDirectTranslations(array $jobs, array $parameters) {
+    $rules_response = array();
+
+    // Checking if there is a node associated with the given job.
+    if ($node = $this->getNodeFromTmgmtJob($jobs[0])) {
+      // Getting the identifier data.
+      $identifier = $this->getIdentifier($jobs[0], $node->nid, $parameters['requester_code']);
+
+      if (isset($identifier['identifier.sequence'])) {
+        // Getting the request data.
+        $data = $this->getRequestData($jobs, $node, $parameters['delay']);
+
+        // Overwrite the data with parameters from 'Rules'.
+        $data = $this->overwriteRequestData($data, $parameters['data']);
+
+        // Sending a review request to DGT Services.
+        $client_action = 'request.create_translation_request';
+        $dgt_response = $this->sendRequest($client_action, $identifier, $data);
+
+        // Process the DGT response to get the Rules response.
+        $jobs[0]->client_action = $client_action;
+        $jobs[0]->client_request_data = $data;
+        $rules_response = $this->processResponse($dgt_response, $jobs);
+      }
+      else {
+        watchdog(
+          'ne_tmgmt_dgt_ftt_translator',
+          "There is an entry in the entity mapping table for a given
+          content. Please make sure that no requests were sent before
+          sending the direct translation request. Node ID: %nid",
+          array('%nid' => $node->nid),
+          WATCHDOG_ERROR
+        );
+      }
+    }
+
+    $rules_response['tmgmt_job'] = $jobs[0];
+
+    return $rules_response;
+  }
+
+  /**
    * Process response from DGT Services.
    *
    * @param \EC\Poetry\Messages\Responses\Status $response
