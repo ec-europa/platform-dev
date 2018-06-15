7240b69efc24f5c7266c69a95c415aa82c49c797
gillesdeudon <gilles.deudon@gmail.com>
2018-06-15 12:37:49 +0200

    NEPT-1432: Add the hook_ecas_user_roles_alter().

--- profiles/common/modules/custom/ecas/ecas.api.php
+++ profiles/common/modules/custom/ecas/ecas.api.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Act on the list of user data value to save of the Ecas user.
+ * Acts on the list of user data value to save of the Ecas user.
  *
  * @param object $user
  *   A user object.
@@ -17,8 +17,7 @@
  *   The list array of user data values. it is the same "$edit" parameter as
  *   in user_save().
  * @param array $args
- *   Extra parameters, not used directly in this function but passed to the
- *   info_ecas_update() hook.
+ *   Extra parameters linked directly to the drupal-Ecas login process.
  */
 function hook_ecas_sync_user_info($user, array $user_info, array &$edit, array $args) {
   $role = user_role_load_by_name('role_name');
@@ -26,6 +25,39 @@ function hook_ecas_sync_user_info($user, array $user_info, array &$edit, array $
   $user_roles[$role->rid] = $role->name;
 
   $edit['roles'] = $user_roles;
+}
+
+/**
+ * Alters the Ecas user roles before saving it.
+ *
+ * @param array $user_roles
+ *   The array of user roles already set.
+ * @param object $user
+ *   The user related to the roles to alter.
+ * @param array $user_info
+ *   An associative array with user data coming from EU login.
+ * @param array $args
+ *   Extra parameters linked directly to the drupal-Ecas login process.
+ */
+function hook_ecas_user_roles_alter(array &$user_roles, $user, array $user_info, array $args) {
+  $user_department = ecas_get_user_department($user_info);
+
+  if (empty($user_department['dg'])) {
+    return;
+  }
+
+  $dg_id = $user_department['dg'];
+
+  // Mapping between DG and drupal roles.
+  $rules = ecas_group_sync_get_ecas_sync_rules($dg_id, 'role');
+
+  if (!$rules) {
+    return;
+  }
+
+  foreach ($rules as $rule) {
+    $user_roles[$rule->synctype_value] = $rule->name;
+  }
 }
 
 /**
@@ -40,8 +72,7 @@ function hook_ecas_sync_user_info($user, array $user_info, array &$edit, array $
  *   An associative array with user data coming from EU login.
  *   These values will be used to fill fields of the user's profiles.
  * @param array $args
- *   Extra parameters, not used directly in this function but passed to the
- *   info_ecas_update() hook.
+ *   Extra parameters linked directly to the drupal-Ecas login process.
  */
 function hook_info_ecas_update($user, array $user_info, array $args) {
   $group_query = db_select('node', 'n')
