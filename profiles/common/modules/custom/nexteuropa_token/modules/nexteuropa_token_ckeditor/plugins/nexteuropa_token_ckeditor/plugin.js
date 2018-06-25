675fb6647b3769c8547ea354b57f6ae4c700eb85
jyraya <gilles.deudon@gmail.com>
2018-06-25 14:48:31 +0200

    NEPT-1989: treat browsers that do not support very well space for CKEditor.

--- profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/plugins/nexteuropa_token_ckeditor/plugin.js
+++ profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/plugins/nexteuropa_token_ckeditor/plugin.js
@@ -33,6 +33,10 @@
             var type = $(this).attr('token-ckeditor-type');
             if (type != 'url') {
               token = token + '{' + Drupal.t('@label as @mode', {'@label': $(this).attr('token-ckeditor-label'), '@mode': $(this).text()}) + '}';
+
+              if (!Drupal.nexteuropa_token_ckeditor.filter.isBrowserSpaceSafe()) {
+                token = '&nbsp;' + token + '&nbsp;';
+              }
             }
             editor.insertHtml(token);
             CKEDITOR.dialog.getCurrent().hide();
@@ -159,7 +163,7 @@
       // Ensure tokens instead the html element is saved.
       editor.on('setData', function (event) {
         var content = event.data.dataValue;
-        event.data.dataValue = Drupal.nexteuropa_token_ckeditor.filter.replaceTokenWithPlaceholder(content);
+        event.data.dataValue = Drupal.nexteuropa_token_ckeditor.filter.replaceTokenWithPlaceholder(content, false);
       });
 
       // Replace tokens with WYSIWYG placeholders.
@@ -171,7 +175,7 @@
       // Replace tokens with WYSIWYG placeholders.
       editor.on('insertHtml', function (event) {
         var content = event.data.dataValue;
-        event.data.dataValue = Drupal.nexteuropa_token_ckeditor.filter.replaceTokenWithPlaceholder(content);
+        event.data.dataValue = Drupal.nexteuropa_token_ckeditor.filter.replaceTokenWithPlaceholder(content, true);
       });
     }
   });
@@ -222,14 +226,26 @@
      *
      * @param content
      *    Text coming from WYSIWYG.
+     * @param insertHtml
+     *    Check if the calling event is insertHtml (true) or not (false).
      *
      * @returns {string}
+     *   Text with placeholders.
      */
-    replaceTokenWithPlaceholder: function (content) {
+    replaceTokenWithPlaceholder: function (content, isInsertHtml) {
       var matches = content.match(this.regex.get_tokens);
       if (matches) {
         for (var i = 0; i < matches.length; i++) {
-          content = content.replace(matches[i], this.getPlaceholderFromToken(matches[i]));
+          var token = matches[i];
+          // Let's replace the ' ' by &nbsp; for placeholder display.
+          // We avoid to do that during the insertHtml event because the
+          // "content" only contains the just inserted tag.
+          // Then, we leave this process while saving the ckeditor data.
+          if (!this.isBrowserSpaceSafe() && !isInsertHtml) {
+            content = content.replace(token + ' ', token + '&nbsp;');
+            content = content.replace(' ' + token, '&nbsp;' + token);
+          }
+          content = content.replace(token, this.getPlaceholderFromToken(token));
         }
       }
       return content;
@@ -239,16 +255,289 @@
      * Replaces placeholders with tokens.
      *
      * @param content
+     *    Text coming from WYSIWYG.
+     *
+     * @returns {string}
+     *   Text with tokens.
      */
     replacePlaceholderWithToken: function (content) {
+      if (!this.isBrowserSpaceSafe()) {
+        // If the browser does manage very well the white space, clean them
+        // before continuing the process.
+        content = Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInPlaceholders(content);
+      }
       var matches = content.match(this.regex.get_placeholders);
       if (matches) {
         for (var i = 0; i < matches.length; i++) {
-          content = content.replace(matches[i], this.getTokenFromPlaceholder(matches[i]));
+          var placeholder = matches[i];
+          content = content.replace(placeholder, this.getTokenFromPlaceholder(placeholder));
         }
       }
       return content;
-    }
+    },
+
+    /**
+     * Check if the used browser managed correctly the white space.
+     *
+     * For more information, see https://dev.ckeditor.com/ticket/12199.
+     *
+     * @return {boolean}
+     *   true if space safe.
+     */
+    isBrowserSpaceSafe: function () {
+      return (!CKEDITOR.env.gecko && !CKEDITOR.env.ie);
+    },
   };
 
+  // Utility class allowing to clean HTML code before saving WYSIWYG content.
+  // It is used when the used browser does not support space correctly.
+  // See https://dev.ckeditor.com/ticket/12199.
+  Drupal.nexteuropa_token_ckeditor.parser = {
+    /**
+     * Clean a HTML element of extra spaces around "nexteuropatoken" tags.
+     *
+     * @param element
+     *   The HTML element to clean
+     *
+     * @return {CKEDITOR.htmlParser.element}
+     *   The cleaned element.
+     */
+    cleanExtraSpaceInElement: function (element) {
+      if ((typeof element.children == 'undefined') || (element.children.length == 0)) {
+        return element;
+      }
+
+      // Clean first extra space of the HTML block.
+      if (this.isWhiteSpace(element.children[0])) {
+        element.children.splice(0, 1);
+      }
+      // Clean last extra space of the HTML block.
+      if (this.isWhiteSpace(element.children[element.children.length - 1])) {
+        element.children.splice((element.children.length - 1), 1);
+      }
+
+      // Clean extra spaces around "nexteuropatoken" tags in the rest of the element.
+      var i = element.children.length;
+      while (i--) {
+        if (element.children[i]['name'] != 'nexteuropatoken') {
+          continue;
+        }
+        // If the "nexteuropatoken" tag is alnone, no space has been detected.
+        // The loop can stop.
+        if ((typeof element.children[i - 1] == 'undefined') && (typeof element.children[i + 1] == 'undefined')) {
+          break;
+        }
+
+        // Clean potential extra space after the "nexteuropatoken" tag.
+        if ((typeof element.children[i + 1] != 'undefined')) {
+          var secondElementAfterTag = (typeof element.children[i + 2] != 'undefined') ? element.children[i + 2] : '';
+          var cleanedAfterTagElement = this.cleanExtraWhiteSpaceAfterTag(element.children[i + 1], secondElementAfterTag);
+
+          if (cleanedAfterTagElement.length != 0) {
+            element.children[i + 1] = cleanedAfterTagElement;
+          }
+          else {
+            element.children.splice((i + 1), 2, ' ');
+          }
+        }
+
+        // Clean potential extra space before the internal link tag.
+        if ((typeof element.children[i - 1] != 'undefined')) {
+          var secondElementBeforeTag = (typeof element.children[i + 2] != 'undefined') ? element.children[i + 2] : '';
+          var cleanedBeforeTagElement = this.cleanExtraWhiteSpaceBeforeTag(element.children[i - 1], secondElementBeforeTag);
+
+          if (cleanedBeforeTagElement.length != 0) {
+            element.children[i - 1] = cleanedBeforeTagElement;
+          }
+          else {
+            element.children.splice((i - 1), 2, ' ');
+          }
+        }
+      }
+      return element;
+    },
+
+    /**
+     * Cleans a HTML content containing "nexteuropatoken" from extra space.
+     *
+     * @param content
+     *   The HMTL content to clean.
+     *
+     * @return {string}
+     *   The cleaned content.
+     */
+    cleanExtraSpaceInPlaceholders: function (content) {
+      var filter = new CKEDITOR.htmlParser.filter({
+        elements: {
+          address: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          article: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          aside: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          blockquote: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          dd: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          div: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          dt: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          fieldset: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          figcaption: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          footer: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          form: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h1: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h2: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h3: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h4: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h5: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          h6: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          header: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          li: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          main: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          nav: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          ol: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          p: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          pre: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          section: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          td: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+          th: function (element) {
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+          },
+        }
+      });
+
+      var fragment = CKEDITOR.htmlParser.fragment.fromHtml(content),
+        writer = new CKEDITOR.htmlParser.basicWriter();
+      filter.applyTo(fragment);
+      fragment.writeHtml(writer);
+      return writer.getHtml();
+    },
+
+    /**
+     * Cleans the element before a HTML tag from extra white space.
+     *
+     * @param elementBeforeTag
+     *   CKEDITOR.htmlParser.element appearing directly before the tag.
+     * @param secondElementBeforeTag
+     *   the second CKEDITOR.htmlParser.element appearing directly before
+     *   the tag.
+     *
+     * @return {CKEDITOR.htmlParser.element|string}
+     *   The tag element cleaned from any extra white space; or an empty string
+     *   if the whole element must be removed.
+     */
+    cleanExtraWhiteSpaceBeforeTag: function (elementBeforeTag, secondElementBeforeTag) {
+      // If the element just before the tag is not a text (value is
+      // "undefined"), there is no extra space before it.
+      if ((typeof elementBeforeTag == 'undefined') && (typeof elementBeforeTag['value'] == 'undefined')) {
+        return '';
+      }
+
+      var firstValue = elementBeforeTag;
+      var secondValue = (typeof secondElementBeforeTag != 'undefined') ? secondElementBeforeTag : '';
+      if (this.isWhiteSpace(firstValue) && this.isWhiteSpace(secondValue)) {
+        return '';
+      }
+
+      // Working on both element did not return results, lt's focus on the first one.
+      elementBeforeTag['value'] = firstValue['value'].replace(/(\s&nbsp;|\s\s|&nbsp;\s)$/, ' ');
+      return elementBeforeTag;
+    },
+
+    /**
+     * Cleans the element after a HTML tag from extra white space.
+     *
+     * @param elementAfterTag
+     *   CKEDITOR.htmlParser.element appearing directly after the tag.
+     * @param secondElementAfterTag
+     *   the second CKEDITOR.htmlParser.element appearing directly after
+     *   the tag.
+     *
+     * @return {CKEDITOR.htmlParser.element|string}
+     *   The tag element cleaned from any extra white space; or an empty string
+     *   if the whole element must be removed.
+     */
+    cleanExtraWhiteSpaceAfterTag: function (elementAfterTag, secondElementAfterTag) {
+      // If the element just after the tag is not a text (value is
+      // "undefined"), there is no extra space before it.
+      if ((typeof elementAfterTag == 'undefined') && (typeof elementAfterTag['value'] == 'undefined')) {
+        return '';
+      }
+
+      var firstValue = elementAfterTag;
+      var secondValue = (typeof secondElementAfterTag != 'undefined') ? secondElementAfterTag : '';
+      if (this.isWhiteSpace(firstValue) && this.isWhiteSpace(secondValue)) {
+        return '';
+      }
+
+      // Working on both element did not return results, lt's focus on the first one.
+      elementAfterTag['value'] = firstValue['value'].replace(/^(\s&nbsp;|\s\s|&nbsp;\s)/, ' ');
+      return elementAfterTag;
+    },
+
+    /**
+     * Check if a CKEDITOR.htmlParser.element is a white space.
+     *
+     * @param element
+     *   The CKEDITOR.htmlParser.element to check.
+     *
+     * @return {boolean}
+     *   true if the element has a ' ' or '&nbsps' value.
+     */
+    isWhiteSpace: function (element) {
+      if (typeof element == 'undefined') {
+        return false;
+      }
+      var value = element['value'];
+      return ((value === ' ' || value == '&nbsp;'))
+    },
+
+  }
 })(jQuery);
