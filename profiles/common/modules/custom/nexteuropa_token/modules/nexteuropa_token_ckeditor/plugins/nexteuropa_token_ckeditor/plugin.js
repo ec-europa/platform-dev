2d57e782830a1e405cb07b1e0ad202e28d4b9165
jyraya <gilles.deudon@gmail.com>
2018-06-26 09:15:22 +0200

    NEPT-1989: Take into account of nested HTML tags in the spaces management of the nexteuropatoken token.

--- profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/plugins/nexteuropa_token_ckeditor/plugin.js
+++ profiles/common/modules/custom/nexteuropa_token/modules/nexteuropa_token_ckeditor/plugins/nexteuropa_token_ckeditor/plugin.js
@@ -264,7 +264,7 @@
       if (!this.isBrowserSpaceSafe()) {
         // If the browser does manage very well the white space, clean them
         // before continuing the process.
-        content = Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInPlaceholders(content);
+        content = Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInPlaceholders(content);
       }
       var matches = content.match(this.regex.get_placeholders);
       if (matches) {
@@ -302,7 +302,7 @@
      * @return {CKEDITOR.htmlParser.element}
      *   The cleaned element.
      */
-    cleanExtraSpaceInElement: function (element) {
+    cleanSpaceInElement: function (element) {
       if ((typeof element.children == 'undefined') || (element.children.length == 0)) {
         return element;
       }
@@ -322,7 +322,7 @@
         if (element.children[i]['name'] != 'nexteuropatoken') {
           continue;
         }
-        // If the "nexteuropatoken" tag is alnone, no space has been detected.
+        // If the "nexteuropatoken" tag is alone, no space has been detected.
         // The loop can stop.
         if ((typeof element.children[i - 1] == 'undefined') && (typeof element.children[i + 1] == 'undefined')) {
           break;
@@ -330,27 +330,19 @@
 
         // Clean potential extra space after the "nexteuropatoken" tag.
         if ((typeof element.children[i + 1] != 'undefined')) {
-          var secondElementAfterTag = (typeof element.children[i + 2] != 'undefined') ? element.children[i + 2] : '';
-          var cleanedAfterTagElement = this.cleanExtraWhiteSpaceAfterTag(element.children[i + 1], secondElementAfterTag);
+          var cleanedAfterTagElement = this.addWhiteSpaceAfterTag(element.children[i + 1]);
 
           if (cleanedAfterTagElement.length != 0) {
             element.children[i + 1] = cleanedAfterTagElement;
-          }
-          else {
-            element.children.splice((i + 1), 2, ' ');
           }
         }
 
         // Clean potential extra space before the internal link tag.
         if ((typeof element.children[i - 1] != 'undefined')) {
-          var secondElementBeforeTag = (typeof element.children[i + 2] != 'undefined') ? element.children[i + 2] : '';
-          var cleanedBeforeTagElement = this.cleanExtraWhiteSpaceBeforeTag(element.children[i - 1], secondElementBeforeTag);
+          var cleanedBeforeTagElement = this.addWhiteSpaceBeforeTag(element.children[i - 1]);
 
           if (cleanedBeforeTagElement.length != 0) {
             element.children[i - 1] = cleanedBeforeTagElement;
-          }
-          else {
-            element.children.splice((i - 1), 2, ' ');
           }
         }
       }
@@ -366,95 +358,98 @@
      * @return {string}
      *   The cleaned content.
      */
-    cleanExtraSpaceInPlaceholders: function (content) {
+    cleanSpaceInPlaceholders: function (content) {
       var filter = new CKEDITOR.htmlParser.filter({
+        text: function (value) {
+          return value;
+        },
         elements: {
           address: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           article: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           aside: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           blockquote: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           dd: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           div: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           dt: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           fieldset: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           figcaption: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           footer: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           form: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h1: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h2: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h3: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h4: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h5: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           h6: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           header: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           li: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           main: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           nav: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           ol: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           p: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           pre: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           section: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           td: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
           th: function (element) {
-            return Drupal.nexteuropa_token_ckeditor.parser.cleanExtraSpaceInElement(element);
+            return Drupal.nexteuropa_token_ckeditor.parser.cleanSpaceInElement(element);
           },
         }
       });
 
-      var fragment = CKEDITOR.htmlParser.fragment.fromHtml(content),
-        writer = new CKEDITOR.htmlParser.basicWriter();
+      var fragment = CKEDITOR.htmlParser.fragment.fromHtml(content);
+      var writer = new CKEDITOR.htmlParser.basicWriter();
       filter.applyTo(fragment);
       fragment.writeHtml(writer);
       return writer.getHtml();
@@ -464,61 +459,98 @@
      * Cleans the element before a HTML tag from extra white space.
      *
      * @param elementBeforeTag
-     *   CKEDITOR.htmlParser.element appearing directly before the tag.
-     * @param secondElementBeforeTag
-     *   the second CKEDITOR.htmlParser.element appearing directly before
-     *   the tag.
+     *   CKEDITOR.htmlParser.element appearing directly before the tag and can
+     *   contain white spaces.
      *
-     * @return {CKEDITOR.htmlParser.element|string}
+     * @return {CKEDITOR.htmlParser.element}
      *   The tag element cleaned from any extra white space; or an empty string
      *   if the whole element must be removed.
      */
-    cleanExtraWhiteSpaceBeforeTag: function (elementBeforeTag, secondElementBeforeTag) {
+    addWhiteSpaceBeforeTag: function (elementBeforeTag) {
       // If the element just before the tag is not a text (value is
       // "undefined"), there is no extra space before it.
-      if ((typeof elementBeforeTag == 'undefined') && (typeof elementBeforeTag['value'] == 'undefined')) {
-        return '';
+      if (typeof elementBeforeTag == 'undefined') {
+        return elementBeforeTag;
       }
 
-      var firstValue = elementBeforeTag;
-      var secondValue = (typeof secondElementBeforeTag != 'undefined') ? secondElementBeforeTag : '';
-      if (this.isWhiteSpace(firstValue) && this.isWhiteSpace(secondValue)) {
-        return '';
+      // If the element is another HTML element, check the last child to see
+      // if it contains a space or not.
+      // If it is a block HTML element, nothing to do.
+      if (typeof elementBeforeTag['name'] != 'undefined'
+        && this.isInlineElement(elementBeforeTag['name'])
+        && (elementBeforeTag.children.length >= 1)) {
+        var lastChildToCheck = elementBeforeTag.children[0];
+        return this.addWhiteSpaceAfterTag(lastChildToCheck);
+      }
+
+      // Keep untouched the element if no value is set.
+      if (typeof elementBeforeTag['value'] == 'undefined') {
+        return elementBeforeTag;
+      }
+
+      if (this.isWhiteSpace(elementBeforeTag)) {
+        return elementBeforeTag;
       }
 
       // Working on both element did not return results, lt's focus on the first one.
-      elementBeforeTag['value'] = firstValue['value'].replace(/(\s&nbsp;|\s\s|&nbsp;\s)$/, ' ');
-      return elementBeforeTag;
+      var value = elementBeforeTag['value'];
+      value = value.replace(/(&nbsp;&nbsp;|\s&nbsp;|\s\s|&nbsp;\s|\s|&nbsp;|)$/, ' ');
+      // Add space after the character that precedes the tag.
+      if (value.match(/[a-zA-Z0-9.,;:!?]$/)) {
+        value += ' ';
+      }
+      value = value.replace(/^¡\s/, '¡');
+      elementBeforeTag['value'] = value.replace(/^¿\s/, '¿');
+      return  elementBeforeTag;
     },
 
     /**
      * Cleans the element after a HTML tag from extra white space.
      *
      * @param elementAfterTag
-     *   CKEDITOR.htmlParser.element appearing directly after the tag.
-     * @param secondElementAfterTag
-     *   the second CKEDITOR.htmlParser.element appearing directly after
-     *   the tag.
+     *   CKEDITOR.htmlParser.element appearing directly after the tag and can
+     *   contain white spaces.
      *
-     * @return {CKEDITOR.htmlParser.element|string}
+     * @return {CKEDITOR.htmlParser.element}
      *   The tag element cleaned from any extra white space; or an empty string
      *   if the whole element must be removed.
      */
-    cleanExtraWhiteSpaceAfterTag: function (elementAfterTag, secondElementAfterTag) {
+    addWhiteSpaceAfterTag: function (elementAfterTag) {
       // If the element just after the tag is not a text (value is
       // "undefined"), there is no extra space before it.
-      if ((typeof elementAfterTag == 'undefined') && (typeof elementAfterTag['value'] == 'undefined')) {
-        return '';
+      if (typeof elementAfterTag == 'undefined') {
+        return elementAfterTag;
       }
 
-      var firstValue = elementAfterTag;
-      var secondValue = (typeof secondElementAfterTag != 'undefined') ? secondElementAfterTag : '';
-      if (this.isWhiteSpace(firstValue) && this.isWhiteSpace(secondValue)) {
-        return '';
+      // If the element is an inline HTML element, check the first child to see
+      // if it contains a space or not.
+      // If it is a block HTML element, nothing to do.
+      if (typeof elementAfterTag['name'] != 'undefined'
+        && this.isInlineElement(elementAfterTag['name'])
+        && (elementAfterTag.children.length >= 1)) {
+        var firstChildToCheck = elementAfterTag.children[0];
+        return this.addWhiteSpaceAfterTag(firstChildToCheck);
+      }
+
+      // Keep untouched the element if no value is set.
+      if (typeof elementAfterTag['value'] == 'undefined') {
+        return elementAfterTag;
+      }
+
+      if (this.isWhiteSpace(elementAfterTag)) {
+        return elementAfterTag;
       }
 
       // Working on both element did not return results, lt's focus on the first one.
-      elementAfterTag['value'] = firstValue['value'].replace(/^(\s&nbsp;|\s\s|&nbsp;\s)/, ' ');
+      var value = elementAfterTag['value'];
+      value = value.replace(/^(&nbsp;&nbsp;|\s&nbsp;|\s\s|&nbsp;\s|\s|&nbsp;)/, ' ');
+      // Clean white space before ending sentence punctuations.
+      if (value.match(/^[a-zA-Z0-9]/)) {
+        value = ' ' + value;
+      }
+      value = value.replace(/^\s\./, '.');
+      value = value.replace(/^\s!/, '!');
+      elementAfterTag['value'] = value.replace(/^\s\?/, '?');
       return elementAfterTag;
     },
 
@@ -538,6 +570,42 @@
       var value = element['value'];
       return ((value === ' ' || value == '&nbsp;'))
     },
+
+    isInlineElement: function (elementName) {
+      var inlineBlockList = [
+        'b',
+        'big',
+        'i',
+        'small',
+        'tt',
+        'abbr',
+        'acronym',
+        'cite',
+        'code',
+        'dfn',
+        'em',
+        'kbd',
+        'strong',
+        'samp',
+        'var',
+        'a',
+        'bdo',
+        'br',
+        'img',
+        'map',
+        'object',
+        'q',
+        'span',
+        'sub',
+        'sup',
+        'button',
+        'input',
+        'label',
+        'select',
+        'textarea',
+      ];
+      return (inlineBlockList.indexOf(elementName) > -1);
+    }
 
   }
 })(jQuery);
