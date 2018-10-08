09871164b88e5783d249fd0f1346f2d8ace7a49f
Anthony Leach <anthonyleach@me.com>
2018-10-08 16:58:20 +0200

    Ensure mock servers are on the 'behat' host.

--- tests/src/Context/ApacheSolrContext.php
+++ tests/src/Context/ApacheSolrContext.php
@@ -67,7 +67,7 @@ class ApacheSolrContext implements Context {
    */
   protected function getServer() {
     if (!$this->server) {
-      $this->server = new Server($this->mockServerPort, 'web');
+      $this->server = new Server($this->mockServerPort, 'behat');
 
       $this->server->start();
 
--- tests/src/Context/FrontendCacheContext.php
+++ tests/src/Context/FrontendCacheContext.php
@@ -80,7 +80,7 @@ class FrontendCacheContext implements Context {
    */
   protected function getServer() {
     if (!$this->server) {
-      $this->server = new Server($this->mockServerPort, 'web');
+      $this->server = new Server($this->mockServerPort, 'behat');
 
       $this->server->start();
 
