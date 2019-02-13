aee4b384c5d4d72965129b047762ecdd3a1a516d
Delphine Lepers <fefaine@gmail.com>
2019-02-13 10:00:06 +0000

    NEPT-2231: Allow iframe and restrict the domains. Add a hook to extend the domains list.

--- profiles/common/modules/custom/multisite_drupal_toolbox/multisite_drupal_toolbox.api.php
+++ profiles/common/modules/custom/multisite_drupal_toolbox/multisite_drupal_toolbox.api.php
@@ -12,3 +12,12 @@ function hook_multisite_drupal_toolbox_filter_options_alter(&$filter_options) {
   // Enable the <object> tag, strongly not advised of course.
   $filter_options['valid_elements']['object'] = array('*' => TRUE);
 }
+
+/**
+ * Alter the list of domains to be whitelisted for your site.
+ */
+function hook_csp_list_alter(&$domains) {
+  // Allow domain1 and domain2.
+  $extra_domains = array('domain1', 'domain2');
+  $domains = array_merge($extra_domains, $domains);
+}
