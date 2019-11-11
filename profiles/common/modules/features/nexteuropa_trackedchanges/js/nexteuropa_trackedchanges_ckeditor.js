381f99a5dba058dd07cdc6279f4d2548c2334bf1
drishu <mr.drishu@gmail.com>
2019-11-11 08:52:55 +0000

    NEPT-2737: Only filter out profiles.

--- profiles/common/modules/features/nexteuropa_trackedchanges/js/nexteuropa_trackedchanges_ckeditor.js
+++ profiles/common/modules/features/nexteuropa_trackedchanges/js/nexteuropa_trackedchanges_ckeditor.js
@@ -7,13 +7,13 @@
   Drupal.behaviors.nexteuropa_trackedchanges_ckeditor = {
     attach: function () {
       $(function () {
-        function filterTracked (event) {
+        function filterTracked(event) {
           var editor = event.editor;
           var editorContent = editor.getData();
           var trackedChanges = editorContent.search('(<span[^>]+class\s*=\s*(")ice-[^>]*>)[^<]*(</span>)');
 
           if (trackedChanges >= 0) {
-            $('select.filter-list option').each(function(index, option) {
+            $('select.filter-list option').each(function (index, option) {
               if (Drupal.settings.tracking_profiles.indexOf(option.value) === -1) {
                 $('select.filter-list option').eq(index).prop('disabled', 'disabled');
               }
