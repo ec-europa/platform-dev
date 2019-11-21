35b2e43f447fb74e9e5ea056c498c1c314cfbf00
drishu <mr.drishu@gmail.com>
2019-11-21 07:26:07 +0000

    NEPT-2745: Implement custom paste filter.

--- profiles/common/modules/features/multisite_wysiwyg/ckeditor/plugins/paste_from_word/filter.js
+++ profiles/common/modules/features/multisite_wysiwyg/ckeditor/plugins/paste_from_word/filter.js
@@ -1,16 +1,15 @@
-
 /**
  * @file
- * afterPasteFromWord
+ * AfterPasteFromWord.
  */
-        
+
 (function ($) {
   Drupal.behaviors.multisite_wysiwyg_paste_from_word = {
     attach: function () {
       $(function () {
         for (var i in CKEDITOR.instances) {
-          CKEDITOR.instances[i].on('paste', function(evt) {
-            // Create a standalone filter
+          CKEDITOR.instances[i].on('paste', function (evt) {
+            // Create a standalone filter.
             var rules = 'em strong abrr img cite blockquote code ul ol li dl dt td br p; a[href]';
             var filter = new CKEDITOR.filter(rules),
             // Parse the HTML string to a pseudo-DOM structure.
@@ -20,10 +19,10 @@
             fragment.writeHtml(writer);
             evt.data.dataValue = writer.getHtml();
         });
-          
+
           CKEDITOR.instances[i].on('afterCommandExec', function (event) {
-            //console.log(event.data.name);
-            //this.filter.disabled = true;
+            // console.log(event.data.name);
+            // this.filter.disabled = true;.
           });
         }
       });
@@ -31,4 +30,3 @@
 
   }
 })(jQuery);
-
