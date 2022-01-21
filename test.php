67ffda749e1c4419d535719524d3cde011889175
Sébastien GERVAIS <gervais.seb@gmail.com>
2022-01-21 16:37:29 +0000

    test

---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+	
+include ("build/vendor/autoload.php");
+use EC\Behat\PoetryExtension\Context\Services\PoetryMock;
+use EC\Poetry\Poetry;
+
+print_r("Start test". PHP_EOL);
+
+$parameters= array(
+	"service" => 
+		array (
+			"host" => "127.0.0.1",
+			"port" => "28999",
+			"endpoint" => "/service",
+			"wsdl" => "/wsdl",
+			"username" => "username",
+			"password" => "password",
+		),
+	"application" => 
+		array (
+			"base_url" => "http://web:8080/build",
+			"endpoint" => "/poetry/notification",
+		),
+	);
+
+$poetry= new Poetry(array());
+$PoetryMock = new PoetryMock($poetry, $parameters);
+
+$saved_parameter = $PoetryMock->getParameters();
+print_r("------ PoetryMock setUp ------". PHP_EOL);
+$PoetryMock->setUp();
+print_r(PHP_EOL);
+print_r(PHP_EOL);
+sleep(1);
+print_r(PHP_EOL);
+print_r(PHP_EOL);
+print_r("------ PoetryMock tearDown ------". PHP_EOL);
+$PoetryMock->setParameters($saved_parameter);
+$PoetryMock->tearDown();
